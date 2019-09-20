a140fcc1c33d289f3b0a42124a4776624e7bc002
TheDevMinerTV <tobigames200@gmail.com>
2019-09-20 18:40:50 +0200

    [Blockchain][Code] Add Transaction struct

--- blockchain/src/main.rs
+++ blockchain/src/main.rs
@@ -1,11 +1,22 @@
 pub struct Block {
-    versionMajor: u8,
-    versionMinor: u8
-    chainKey: String,
-    prevHash: String,
+    version_major: u8,
+    version_minor: u8,
+    chain_key: String,
+    prev_hash: String,
     timestamp: u64,
     hash: String,
     txns: Vec<Transaction>,
     signature: String,
-    nodeSignature String,
+    node_signature: String,
+}
+
+pub struct Transaction {
+    amount: u64,
+    sender_key: String,
+    reciever_key: String,
+    unlock_time: u64,
+    gas_price: u64,
+    max_gas: u64,
+    nonce: u64,
+    signature: String,
 }
