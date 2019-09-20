0968a585bb825d62a4eacd456c6eb5bcae2ab525
TheDevMinerTV <tobigames200@gmail.com>
2019-09-20 21:14:32 +0200

    [Config] Filename extension of Rust is .rs

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -4,7 +4,7 @@ pub struct Config {
     version_major: u32,
     version_minor: u32,
     coin_name: String,
-    node_drop_off_threshhold: u64, // percent of online nodes that can go offline before a rearrange
+    node_drop_off_threshold: u64, // percent of online nodes that can go offline before a rearrange
     decimal_places: u8,
     max_connections: u16,
     max_threads: u8,
@@ -13,7 +13,12 @@ pub struct Config {
     host: u64,
 }   
 
-fn config() -> Config {
-  var config = Config {version_major: 1, version_minor: 0, coin_name: "aviro", node_drop_off_threshhold: 40, decimal_places: 4}
-  config
+pub fn config() -> Config {
+  Config {
+    version_major: 1,
+    version_minor: 0,
+    coin_name: "aviro",
+    node_drop_off_threshhold: 40,
+    decimal_places: 4
+  };
 }
