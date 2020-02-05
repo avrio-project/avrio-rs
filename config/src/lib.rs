4281802dafae46023d3862980280514ee16d4034
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-05 16:49:27 +0000

    [Config] read from node.conf

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,5 +1,8 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use serde::{Serialize, Deserialize};
 
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     version_major: u32,
     version_minor: u32,
@@ -11,14 +14,17 @@ pub struct Config {
     chain_key: String,
     state: u8,
     host: u64,
-}   
+    seednodes: Vec<Vec<u8>>,
+    ignore_minor_updates: bool,
+    p2p_port: u16,
+    rpc_port: u16,
+    allow_cors: String,
+}  
 
 pub fn config() -> Config {
-  Config {
-    version_major: 1,
-    version_minor: 0,
-    coin_name: "aviro",
-    node_drop_off_threshhold: 40,
-    decimal_places: 4
-  };
+  let mut file = File::open("node.conf").unwrap();
+    let mut data: String = "";
+    file.read_to_string(&mut data).unwrap();
+    let conf: Config = serde_json::from_str(&data).unwrap();
+    return conf;
 }
