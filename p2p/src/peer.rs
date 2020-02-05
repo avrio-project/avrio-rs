53503270de5b254705db6d598ec8df9cf9be4d50
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-05 17:05:48 +0000

    [P2p] edit p2p code

--- p2p/src/peer.rs
+++ p2p/src/peer.rs
@@ -6,6 +6,8 @@ use crate::config::*;
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::fs::*;
+use std::io::prelude::*;
 
 pub struct Peer {
     id: Id,
@@ -20,7 +22,7 @@ pub struct P2pdata {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let mut self_config = config().unwrap();
+    let mut self_config : Config = config().unwrap();
     let buffer_bytes = self_config.buffer_bytes;
     let mut data = [0 as u8; buffer_bytes]; // using a config_self.buffer_bytes buffer
     while match stream.read(&mut data) {
@@ -43,12 +45,19 @@ fn handle_client(mut stream: TcpStream) {
 fn rec_server() -> u8{
     let mut self_config = config();
     let listener = TcpListener::bind(self_config.bind_socket).unwrap();
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("peerlist.aio")
+        .unwrap();
     // accept connections and process them, spawning a new thread for each one
     println!("[INFO] P2p incoming server launched on {0}", self_config.bind_socket);
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("[INFO] New connection: {0}", stream.peer_addr().unwrap());
+                writeln!(file, stream.peer_addr().unwrap());
+                println!("[INFO] New incoming connection: {0}", stream.peer_addr().unwrap());
+                
                 thread::spawn(move|| {
                     // connection succeeded
                     handle_client(stream)
@@ -70,15 +79,10 @@ fn new_connection(socket: Socket) -> Peer { // This Fucntion handles all the det
     let self_config = config().unwrap();
     /*Once we have established a connection over TCP we now send vital data as a hanshake,
     This is in the following format 
-    network id,our peer id, their peer id, our block count, our top block hash, network id;
-    The recipitent then verifyes this and if their block count is lower they send the same back foloowed by a sync a sync request,
-    If they are are equal to or higher than our block count then they verify that the top bock hash is correct, if it is they send
-    the same hand shake back to us;
+    0x1a, network id,our peer id, their peer id, our node type, ox1b;
+    The recipitent then verifyes this then they send the same hand shake back to us;
     */
-    if(!fullNode()) {
-        let mut msg = config.network_id + "," + serde_json::to_string(&self_config.identity).unwrap() + "," + Peer + "," + getBlockHeight() + "," + getTopBlock().hash + "," + config.network_id+ ";";
-    }else {
-        let mut msg = config.commitee_id + "," + serde_json::to_string(&self_config.identity).unwrap() + "," + Peer + "," + getBlockHeight() + "," + getTopBlock().hash + "," + config.network_id+ ";";
+    let mut msg = "0x1a," + config.network_id + "," + serde_json::to_string(&self_config.identity).unwrap() + "," + Peer + "," + self_config.node_type + "," + config.network_id+ "0x1b";
     stream.write(serde_json::to_string(msg); // send our handshake
     stream.read(&mut [0; 128])?;
     Ok(())
