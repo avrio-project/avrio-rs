52d0c9a683bb24ddf08eed9dc3700819c1d6099f
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-06 05:23:08 +0000

    [CORE][GENESIS]

---
+++ core/src/genesis.rs
@@ -0,0 +1,7 @@
+// This file generates the genesis block for a new network.
+
+extern crate database;
+extern crate blockchain;
+extern crate config;
+
+let mut genesis_txns: Vec<Transactions> = [];
