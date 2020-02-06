194b44a76b4b6685fea437cabc5c49b1029f0855
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-06 05:44:46 +0000

    [CORE] [GENESIS] Generate Genesis Block Code

--- core/src/genesis.rs
+++ core/src/genesis.rs
@@ -3,5 +3,43 @@
 extern crate database;
 extern crate blockchain;
 extern crate config;
+extern crate rand;
+use rand::Rng;
+extern crate hex;
 
-let mut genesis_txns: Vec<Transactions> = [];
+let mut genesis_txns: Vec<Transactions> = vec![
+    Transaction {
+        amount: 0,
+        extra: "",
+        flag: 0x00,
+        sender_key: hex::encode(vec![0, 32]).to_owned(),
+        receive_key: "",
+        access_key: Option::None,
+        gas_price: 1,
+        gas: 0,
+        max_gas: u64::max_value(),
+        nonce: rng.gen::<u8>(),
+    }
+];
+
+fn generateGenesisBlock() -> Block{
+    for tx in genesis_txns {
+        tx.hash = tx.hash();
+        tx.signature = sign(hex::encode(vec![0, 32]).to_owned(), tx.hash);
+    }
+    let mut genesis_block = Block {
+        header: Header {
+            version_major: 0,
+            version_minor: 0
+            chain_key: hex::encode(vec![0, 32]).to_owned(),
+            prev_hash: hex::encode(vec![0, 32]).to_owned(),
+            height: 0,
+            timestamp: 0,
+        },
+        txns: genesis_txns,
+    }
+    genesis_block.hash = genesis_block.hash();
+    genesis_block.signature = genesis_block.sign(hex::encode(vec![0, 32]).to_owned(), genesis_block.hash);
+    return genesis_block;
+}
+        
