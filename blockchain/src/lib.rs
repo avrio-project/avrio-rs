624ae635b4ba8a65d5b8e0afce13a02f3dca2b09
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-06 05:51:15 +0000

    [BLOCKCHAIN] Make blocks hashable

--- blockchain/src/lib.rs
+++ blockchain/src/lib.rs
@@ -22,6 +22,35 @@ pub struct Block {
     node_signatures:Vec<String>, // a block must be signed by at least (c / 2) + 1 nodes to be valid (ensures at least ne honest node has singed it)
 }
 
+impl Hashable for Header {
+    fn bytes (&self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        bytes.extend(self.version_major);
+        bytes.extend(self.version_minor);
+        bytes.extend(self.chain_key);
+        bytes.extend(self.prev_hash);
+        bytes.extend(self.receive_key);
+        bytes.extend(self.height); 
+        bytes.extend(self.timestamp);
+        bytes
+    }
+}
+
+impl Hashable for Block {
+    fn bytes (&self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        bytes.extend(self.header.bytes());
+        bytes.extend(
+            self.txns
+                .iter()
+                .flat_map(|Transaction| Transaction.bytes())
+                .collect::<Vec<u8>>()
+        );
+        bytes
+    }
+}
 
 pub fn check_block(blk: Block) -> bool {
     if blk.header.version_major > config.version_major {
