b3a2a1c5f63a90de260eb9f170024c42ee68ba69
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-06 04:57:38 +0000

    [BLOCKCHAIN] Moving transaction to core

--- blockchain/src/lib.rs
+++ blockchain/src/lib.rs
@@ -4,49 +4,6 @@ extern crate crypto;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Transaction {
-    hash: String,
-    amount: u64,
-    extra: String,
-    flag: char,
-    sender_key: String,
-    receive_key: String,
-    access_key: String,
-    gas_price: u64,
-    max_gas: u64,
-    gas: u64, // gas used
-    nonce: u8,
-    signature: String,
-}
-impl Hashable for Transaction { // TXN CREATION 101: run this then do tx.hash(); then sign the hash provided
-    fn bytes (&self) -> Vec<u8> {
-        let mut bytes = vec![];
-
-        bytes.extend(self.ammount);
-        bytes.extend(self.extra);
-        bytes.extend(self.flag);
-        bytes.extend(self.sender_key);
-        bytes.extend(self.receive_key);
-        bytes.extend(self.gas * self.gas_price); // aka fee
-        bytes.extend(self.nonce);
-        bytes
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct TxStore { // remove data not needed to be stored
-    hash: String,
-    amount: u64,
-    extra: String,
-    sender_key: String,
-    receive_key: String,
-    access_key: String,
-    fee: u64, // fee in AIO (gas_used * gas_price)
-    nonce: u8,
-    signature: String,
-}    
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct Header {
     version_major: u8,
     version_minor: u8,
@@ -86,6 +43,3 @@ pub fn check_block(blk: Block) -> bool {
     return false;
 }
 
-fn hashTransaction(tx: Transaction) -> String {
-    return tx.hash();
-}
