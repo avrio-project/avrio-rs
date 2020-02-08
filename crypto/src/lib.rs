77228941fbee2ca7dd86b1bf099c107f3e8aebc0
TheDevMinerTV <tobigames200@gmail.com>
2020-02-08 17:20:50 +0100

    [Crypto] Format

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,34 +1,23 @@
-
 use libsodium_sys::{
-    sodium_init,
-    randombytes_buf,
-    crypto_box_PUBLICKEYBYTES,
-    crypto_box_SECRETKEYBYTES,
-    crypto_box_keypair,
-    crypto_scalarmult,
-    crypto_secretbox_easy,
-    crypto_secretbox_open_easy,
-    crypto_secretbox_MACBYTES,
-    sodium_memzero,
+    crypto_box_PUBLICKEYBYTES, crypto_box_SECRETKEYBYTES, crypto_box_keypair, crypto_scalarmult,
+    crypto_secretbox_MACBYTES, crypto_secretbox_easy, crypto_secretbox_open_easy, randombytes_buf,
+    sodium_init, sodium_memzero,
 };
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct keypair{
+pub struct keypair {
     publickey: [u8; crypto_box_PUBLICKEYBYTES],
     privatekey: [u8; crypto_box_SECRETKEYBYTES],
 }
 
-fn generateKeys()-> keypair {
+fn generateKeys() -> keypair {
     let out: keypair;
     if (sodium_init() == -1) {
         return out;
-    }else {
+    } else {
         crypto_box_keypair(out.publickey, out.privatekey);
         return out;
     }
-
 }
 
-fn hash(msg: String) -> String {
-    
-}
+fn hash(msg: String) -> String {}
