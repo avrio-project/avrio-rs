cc2a120e6e1ad2fc1c13245b8808a7e7619ead1f
TheDevMinerTV <tobigames200@gmail.com>
2020-02-08 17:20:39 +0100

    [Crypto][Hashable] Format

--- crypto/src/hashable.rs
+++ crypto/src/hashable.rs
@@ -1,11 +1,11 @@
 use cryptonight::*;
 
 pub trait Hashable {
-    fn bytes (&self) -> Vec<u8>;
+    fn bytes(&self) -> Vec<u8>;
 
-    fn hash (&self) -> Vec<u8> {
+    fn hash(&self) -> Vec<u8> {
         if !cryptonight::getHashingParams().memory {
-            cryptonight::setHAshingParams(i,m); // needs defult value setting
+            cryptonight::setHAshingParams(i, m); // needs defult value setting
         }
 
         cryptonight::cryptonight(&self.bytes(), self.bytes().len(), 0);
