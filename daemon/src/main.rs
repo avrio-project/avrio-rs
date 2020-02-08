884701ad952a5e436efbfc7727eba8cc50fb95f1
TheDevMinerTV <tobigames200@gmail.com>
2020-02-08 16:36:01 +0100

    [Daemon][Main] Fix issues

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -2,25 +2,26 @@ extern crate ansi_term;
 
 use ansi_term::Colour;
 
-fn red(text) {
+fn red(text: String) {
     println!(Colour::Red.paint(text))
 }
-fn blue(text) {
+fn blue(text: String) {
     println!(Colour::Blue.paint(text))
 }
-fn green(text) {
+fn green(text: String) {
     println!(Colour::Green.paint(text))
 }
-fn white(text) {
+fn white(text: String) {
     println!(Colour::White.paint(text))
 }
-fn yellow(text) {
+fn yellow(text: String) {
     println!(Colour::Yellow.paint(text))
 }
 
 fn main() {
-    white('test white');
-    red('test red');
-    blue('test blue');
-    white('test white repeat');
-    green('test green');
+    white("test white");
+    red("test red");
+    blue("test blue");
+    white("test white repeat");
+    green("test green");
+}
