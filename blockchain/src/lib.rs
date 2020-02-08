1cab8304261edd27b093cf19f006ee01f9e4ff0d
TheDevMinerTV <tobigames200@gmail.com>
2020-02-08 17:19:29 +0100

    [Blockchain] Format and fix

--- blockchain/src/lib.rs
+++ blockchain/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate config;
 extern crate core;
 extern crate crypto;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Header {
@@ -19,11 +19,11 @@ pub struct Block {
     txns: Vec<Transaction>,
     hash: String,
     signature: String,
-    node_signatures:Vec<String>, // a block must be signed by at least (c / 2) + 1 nodes to be valid (ensures at least ne honest node has singed it)
+    node_signatures: Vec<String>, // a block must be signed by at least (c / 2) + 1 nodes to be valid (ensures at least ne honest node has singed it)
 }
 
 impl Hashable for Header {
-    fn bytes (&self) -> Vec<u8> {
+    fn bytes(&self) -> Vec<u8> {
         let mut bytes = vec![];
 
         bytes.extend(self.version_major);
@@ -31,14 +31,14 @@ impl Hashable for Header {
         bytes.extend(self.chain_key);
         bytes.extend(self.prev_hash);
         bytes.extend(self.receive_key);
-        bytes.extend(self.height); 
+        bytes.extend(self.height);
         bytes.extend(self.timestamp);
         bytes
     }
 }
 
 impl Hashable for Block {
-    fn bytes (&self) -> Vec<u8> {
+    fn bytes(&self) -> Vec<u8> {
         let mut bytes = vec![];
 
         bytes.extend(self.header.bytes());
@@ -46,24 +46,26 @@ impl Hashable for Block {
             self.txns
                 .iter()
                 .flat_map(|Transaction| Transaction.bytes())
-                .collect::<Vec<u8>>()
+                .collect::<Vec<u8>>(),
         );
         bytes
     }
 }
 
 pub fn check_block(blk: Block) -> bool {
-    if blk.header.height == 0 { // genesis block
+    if blk.header.height == 0 {
+        // genesis block
         if blk != generateGenesisBlock() {
-            return false
-        }
-    } else { // not genesis block
-        if blk.header.prev_hash != get_block(blk.header.chain_key, blk.header.height -1) {
-            return false;
-        } else if !check_signature(blk.signature,blk.header.chain_key) {
             return false;
         }
-    
+    } else {
+        // not genesis block
+        if blk.header.prev_hash != get_block(blk.header.chain_key, blk.header.height - 1) {
+            return false;
+        } else if !check_signature(blk.signature, blk.header.chain_key) {
+            return false;
+        }
+
         for txn in blk.txns {
             if !validate_transaction(txn) {
                 return false;
@@ -72,4 +74,3 @@ pub fn check_block(blk: Block) -> bool {
         return false;
     }
 }
-
