54beed3e5ac9d062a3a484ad698a5cb599b5e9c5
TheDevMinerTV <tobigames200@gmail.com>
2020-02-08 17:08:54 +0100

    [ID] Format and fix

--- id/src/lib.rs
+++ id/src/lib.rs
@@ -1,12 +1,13 @@
 // This lib deals with the generation of ID's based off the random strings provided by the consensius commitee at the end of the last round
-use std::io::{stdin,stdout,Write};
+use std::io::{stdin, stdout, Write};
 use std::time::{SystemTime, UNIX_EPOCH};
 extern crate rand;
+
 use rand::Rng;
 
 struct HashParams {
     iterations: u32,
-    memory: u32
+    memory: u32,
 }
 
 struct IdDetails {
@@ -14,7 +15,7 @@ struct IdDetails {
     signed: String,
     nonce: u64,
     start_t: u64,
-    end_t: u64
+    end_t: u64,
 }
 
 pub fn difficulty_bytes_as_u128 (v: &Vec<u8>) -> u128 {
@@ -41,57 +42,72 @@ pub fn check_difficulty (hash: &Hash, difficulty: u128) -> bool {
 }
 
 fn calculate_hash_params(PrevBlockHash: String) -> HashParams {
-  let mut cu = PrevBlockHash.as_bytes();
-  let mut b: Vec<u8> = cu.iter().cloned().collect();
-  let mut a: u32 =0;
-  let mut i =0;
-  for x in &b {
-    a = a + *x as u32;
-  }
-  return HashParams{iterations: a * 10, memory: a * 20);  
+    let mut cu = PrevBlockHash.as_bytes();
+    let mut b: Vec<u8> = cu.iter().cloned().collect();
+    let mut a: u32 = 0;
+    let mut i = 0;
+
+    for x in &b {
+        a = a + *x as u32;
+    }
+
+    return HashParams(iterations: a * 10, memory: a * 20);
 }
 
 fn hash_string(params: HashParams, s: String) -> String {
-  unsafe 
-  {
-    let in = s.as_bytes();
-    cryptonight::set_params(params.memory, params.iterations);
-    let out = cryptonight(&in, in.len(), 0);
-    let mut out_abs: Vec<u8> = vec![];
-    for x in out {
-        if x < 0 {
-            let x_pos = x + (x*2);
-            out_abs.push(x_pos);
-        }else {
-            out_abs.push(x);
+    unsafe {
+        let input = s.as_bytes();
+        cryptonight::set_params(params.memory, params.iterations);
+        let out = cryptonight(&input, input.len(), 0);
+        let mut out_abs: Vec<u8> = vec![];
+
+        for x in out {
+            if x < 0 {
+                let x_pos = x + (x * 2);
+                out_abs.push(x_pos);
+            } else {
+                out_abs.push(x);
+            }
         }
+
+        return hex::encode(out_abs);
     }
-    return hex::encode(out_abs);
-  }
 }
 
+pub fn generateId(
+    k: String,
+    public_key: String,
+    private_key: String,
+    difficulty: u128,
+) -> IdDetails {
+    let mut struct_: IdDetails;
+    let params = calculate_hash_params(getLastBlockHash());
+    let mut nonce: u32 = 0;
+    let mut hashed: String = "";
+
+    struct_.start_t = SystemTime::now().as_millis();
+
+    while true {
+        nonce = nonce + 1;
+        hashed = hash_string(params, k + public_key + nonce);
+
+        // check difficulty
+        if check_difficulty(&hashed, difficulty) {
+            struct_.nonce = nonce;
+            struct_.hash = hashed;
+            struct_.end_t = SystemTime::now().as_millis();
+            println!(
+                "[INFO] Found ID hash: {0} with nonce: {1} (in {2} secconds)",
+                hashed,
+                nonce,
+                (struct_.end_t - start_.start_t) / 1000
+            );
+            break;
+        }
+    }
+
+    struct_.signed = sign(hashed, private_key);
+
+    return struct_;
 }
-pub fn generateId(k: String, public_key: String, private_key, difficulty: u128) -> IdDetails
-{
-  let mut struct_ =  new IdDetails;
-  let params = calculate_hash_params(getLastBlockHash());
-  let mut nonce: u32 = 0;
-  let mut hashed: String = "";
-  struct_.start_t = SystemTime::now().as_millis();
-  while true {
-      nonce = nonce + 1;
-      hashed = hash_string(params, k + public_key + nonce);
-      // check difficulty
-      if (check_difficulty(&hashed, difficulty) {
-          struct_.nonce = nonce;
-          struct_.hash = hashed;
-          struct_.end_t = SystemTime::now().as_millis();
-          println!("[INFO] Found ID hash: {0} with nonce: {1} (in {2} secconds)",hashed,nonce, (struct_.end_t - start_.start_t) / 1000);
-          break;
-      }
-  }
-  
-  struct_.signed = sign(hashed,private_key);
-  return struct_;
-}
-  
+
