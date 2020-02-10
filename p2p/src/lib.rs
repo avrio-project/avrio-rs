99cd80f9670b01b6625db47d0d18e99f1756fd1d
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-10 19:07:55 +0000

    [P2P] Move to tokio.rs

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -1,13 +1,34 @@
-use libp2p::*; // evil but easy 
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use futures::stream::StreamExt;
+
 pub enum p2p_errors {
   None,
   TimeOut,
   InvalidMultiAdrr,
   Other
 }
-function connect(peer: &Multiaddr) {
-  let tcp = TcpConfig::new();
-  let _conn = tcp.dial(peer.parse().expect("invalid multiaddr"););
-  let mut error: p2p_errors = p2p_errors::None;
-  return error;
+
+pub function launchP2pServer(addr: &String, port: &16) {
+  let address = String::from(addr + ":" + port);
+  let mut listener = TcpListener::bind(address).await.unwrap();
+  let server = async move {
+        let mut incoming = listener.incoming();
+        while let Some(socket_res) = incoming.next().await {
+            match socket_res {
+                Ok(socket) => {
+                    println!("[INFO] Accepted connection from {:?}", socket.peer_addr());
+                    let (mut reader, mut writer) = sock.split();
+                    handleConn(reader, writer);
+                }
+                Err(err) => {
+                    // Handle error by printing to STDOUT.
+                    println!("[WARN] Error accepting peer {:?}, threw error = {:?}",socket.peer_addr(), err);
+                }
+            }
+        }
+    };
+    thread::Builder::new().name("p2pServer".to_string()).spawn( move || {
+      server.await;
+    }
 }
