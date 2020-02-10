f71d6f7cf669ed2f8c240b4f4ba343d17c27ca84
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-10 18:02:43 +0000

    [P2P] Begin new code

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -1,2 +1,13 @@
-mod peer;
-mod tracker;
+use libp2p::*; // evil but easy 
+pub enum p2p_errors {
+  None,
+  TimeOut,
+  InvalidMultiAdrr,
+  Other
+}
+function connect(peer: &Multiaddr) {
+  let tcp = TcpConfig::new();
+  let _conn = tcp.dial(peer.parse().expect("invalid multiaddr"););
+  let mut error: p2p_errors = p2p_errors::None;
+  return error;
+}
