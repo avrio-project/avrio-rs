c3d5981a75901e7b602dcf9bc6c0987e292fbfd5
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-11 00:19:21 +0000

    [P2P] Update msg handling

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -5,42 +5,44 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct P2pdata {
-    message_bytes: u64, // The length in bytes of this message
+    message_bytes: u64, // The length in bytes of message
+    message_type: byte, // The type of data
     message: String,   // The serialized data
 }
 
 pub enum p2p_errors {
   None,
   TimeOut,
-  InvalidMultiAdrr,
-  Other
+  InvalidSocket,
+  Other,
 }
-function formMsg(data_s: String, network_id: Vec<byte>) -> Vec<bytes> {
-    let mut msg: Vec<bytes>;
-    for byte_ in network_id {
-        msg.push(byte);
-    }
-    for byte_ in data_s.as_bytes() {
-        msg.push(byte_);
-    }
-    msg.push(0x00);
-    return msg;
+
+function formMsg(data_s: String, data_type: byte) -> String {
+    let mut msg: P2pdata = {
+        message_bytes: data_s.len(),
+        message_type: data_type,
+        message: data_s,
+    };
+    return serde_json::to_string(&msg).unwrap();
 }
-function handleConn(sock: UdpSocket) {
+
+function deformMsg(&msg: String) -> u8 { // deforms message and excutes appropriate function to handle resultant data
+    let mut msg_d:P2pdata = serde_json::from_string(msg).unwrap();
+    match msg_d.message_type {
+        "0x0a" => process_block(msg_d.message) => return 1, // block
+        "0x0b" => process_transaction(msg_d.message) => return 2,
+        "0x0c" => process_registration(msg_d.message) => return 3,
+        "0x1a" => process_handshake(msg_d.message) => return 4,
+        _ => println!("[WARN] Bad Messge type from peer. Message type {:?}. (If you ae getting,losts of these check for updates)", msg_d.message_type) => return 0,
+    }
+}
+
+function handleConn(&sock: UdpSocket) {
   let (mut reader, mut writer) = sock.split(); 
   let data = reader.blah(); // todo
-  match data[0..4] {
-    "0x0a" => let mut block: Block, // block
-    "0x0b" => let mut txn: Transaction,
-    "0x0c" => let mut certificate: Certificate,
-  }
+  deformMsg(&data);
 }
-/* msg format
-network id
-msg type (eg 0x1b = block)
-serilised P2pdata
-0x00 (null)
-*/
+
 pub function launchP2pServer(addr: &String, port: &16) {
   let address = String::from(addr + ":" + port);
   let mut listener = TcpListener::bind(address).await.unwrap();
@@ -50,6 +52,7 @@ pub function launchP2pServer(addr: &String, port: &16) {
             match socket_res {
                 Ok(socket) => {
                     println!("[INFO] Accepted connection from {:?}", socket.peer_addr());
+                    // Now handle the message they send to us
                     handleConn(socket);
                 }
                 Err(err) => {
