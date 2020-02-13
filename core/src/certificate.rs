a373bd526ab13a94cfd3e9e5c1406d905946face
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-13 21:17:54 +0000

    [CORE][CERTIFICATE] Continue code
    TODO: Add hashing, hex::encode bytes

--- core/src/certificate.rs
+++ core/src/certificate.rs
@@ -3,6 +3,7 @@ This file handles the generation, validation and saving of the fullnodes certifi
 /*
 
 extern crate hex;
+use std::time::{Duration, SystemTime};
 
 enum certificateGenerationErrors {
   transactionNotFound,
@@ -23,6 +24,15 @@ pub struct Certificate {
   pub timestamp: u64,
   pub signature: String,
 }
+pub fn generateCertificate(pk: &String, privateKey: &String, txnHash: &String) -> Result<certificateGenerationErrors> {
+  let mut cert: Certificate = Certificate { };
+  cert.publicKey = pk;
+  cert.txnHash = txnHash;
+  cert.timestamp = SystemTime::now();
+  for nonce in u64::maxValue() {
+    //
+  }
+}
 impl Certificate {
   fn encodeForFile(&self) -> Vec<u8>{
     let mut bytes = vec![];
@@ -32,6 +42,15 @@ impl Certificate {
     bytes.extend(self.nonce.to_owned().to_string().bytes());
     bytes.extend(self.timestamp.to_owned().to_string().bytes());
     bytes.extend(self.signature.bytes());
+    bytes
+  }
+  
+  fn encodeForHash(&self) -> Vec<u8>{
+    let mut bytes = vec![];
+    bytes.extend(self.publicKey.bytes());
+    bytes.extend(self.txnHash.bytes());
+    bytes.extend(self.nonce.to_owned().to_string().bytes());
+    bytes.extend(self.timestamp.to_owned().to_string().bytes());
     bytes
   }
 }
