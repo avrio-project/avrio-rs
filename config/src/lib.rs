c2bb51b7b06d54fab89891e1a4e9ef363d77476b
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-18 13:45:36 +0000

    [CONFIG] Add new params and functions

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -9,34 +9,53 @@ pub struct Config {
     pub version_major: u32,
     pub version_minor: u32,
     pub coin_name: String,
-    pub node_drop_off_threshold: u64, // percent of online nodes that can go offline before a rearrange
+    pub node_drop_off_threshold: u64,
     pub decimal_places: u8,
     pub max_connections: u16,
     pub max_threads: u8,
     pub chain_key: String,
     pub state: u8,
-    pub host: u64,
+    pub ip_host: Vec<u8>,
     pub seednodes: Vec<Vec<u8>>,
     pub ignore_minor_updates: bool,
     pub p2p_port: u16,
     pub rpc_port: u16,
-    pub allow_cors: String,
+    pub allow_cors: char,
     pub buffer_bytes: u16,
-    pub network_id: String,
+    pub network_id: Vec<u8>,
     pub node_type: char,
     pub identitiy: String,
+    pub key_file_path: String,
+    pub log_level: u8, 
 }
 
 pub fn config() -> Config {
-    let mut file = File::open("node.conf").unwrap();
+    let mut file = File::open("node.conf").unwrap_or_else(|e| {
+        errror!("Failed to Open Config file: {}", e);
+        panic!();
+    });
     let mut data: String = String::from("");
     file.read_to_string(&mut data).unwrap();
-    let conf: Config = serde_json::from_str(&data).unwrap();
+    let conf: Config = serde_json::from_str(&data).unwrap_or_else(|e| {
+        error!("Failed To Deserilise Config: {}", e);
+        panic!();
+    });
     return conf;
 }
-
-pub fn create_config(conf: Config) -> io::Result<()> {
-    let mut file = File::create("node.conf")?;
-    file.write_all(serde_json::to_string(&conf).unwrap().as_bytes())?;
-    Ok(())
+impl Config {
+    pub fn new_default() -> Config { // creates a config struct with default value (using Config::new_default()) // TODO
+        return Config {
+            //
+        };
+    }
+    pub fn create(&self) -> io::Result<()> { // create file
+        let mut file = File::create("node.conf")?;
+        file.write_all(serde_json::to_string(&conf).unwrap().as_bytes())?;
+        Ok(())
+    }
+    pub fn save(&self) -> io::Result<()> { // save to exisiting/ update
+        let mut file = File::open("node.conf")?;
+        file.write_all(serde_json::to_string(&conf).unwrap().as_bytes())?;
+        Ok(())
+    }
 }
