a7a9c933a4b1e32c0c8617dff41a8251550e6c1b
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-19 21:18:59 +0000

    [DATABASE] Update Code

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,11 +1,15 @@
 use rocksdb::{Options, DB};
 use serde::{Deserialize, Serialize};
 extern crate config;
-extern crate core;
+extern crate core
+use core::{TxStore, Account};
+extern crate log;
 
 fn saveData(serialized: String, path: String, key: String) -> u8 {
     // used to save data without having to create 1000's of functions (eg saveblock, savepeerlist, ect)
-    let db = DB::open_default(path).unwrap();
+    let db = DB::open_default(path).unwrap_or_else(|e| { 
+        fatal!("Failed to open database at path {}, gave error {:?}", path, e);
+    });
     db.put(key, serialized);
     return 1;
 }
@@ -16,7 +20,7 @@ fn savePeerlist(list: &Vec<Vec<u8>>, path) {
     let ip_curr: String;
     for ip in list {
         for ip_seg in ip {
-            ip_curr = &ip_curr + "." + String::from(ip_seg);
+            ip_curr = &ip_curr + ".".to_owned() + &String::from(ip_seg);
         }
         ips.push(String:from(ip_curr));
     }
@@ -28,27 +32,40 @@ fn getData(path: String, key: String) -> String {
     let mut data: String;
     match db.get(key) {
         Ok(Some(value)) => data = value,
-        Ok(None) => data = "-1",
-        Err(e) => data = "0",
+        Ok(None) => data = "-1".to_owned(),
+        Err(e) => data = "0".to_owned(),
     }
-    return data.to_string();
+    return data;
 }
 
 fn setAccount(acc: Account) -> u8 {
-    let path = config.path + "/db/accountdb";
-    let serialized = serde_json::to_string(&acc).unwrap();
+    let path = config().db_path + "/db/accountdb";
+    let serialized: String;
+    serialized = serde_json::to_string(&acc).unwrap_or_else(|e| {
+        error!("Unable To Serilise Account, gave error {}, retrying", e);
+        serialized = serde_json::to_string(&acc).unwrap_or_else(|et| { 
+                fatal!("Retry Failed with error: {}" et);
+                panic!();
+        });
+    });
     saveData(serialized, path, acc.public_key);
     return 1;
 }
 
-fn getAccount(public_key: String) -> Account {
-    let path = config.path + "/bd/accountdb";
+fn getAccount(public_key: String) -> Result<Account, u8> {
+    let path = config().db_path + "/bd/accountdb".to_owned();
     let mut data = getData(path, public_key);
-    let db = DB::open_default(path).unwrap();
-    if state != "1" {
-        return nullAcc;
+    if data != "1" {
+        return Err(1);
     } else {
-        let acc = serde_json::from_str(&data).unwrap();
+        let acc: Account;
+        acc = serde_json::from_str(&data).unwrap_or_else(|e| { 
+            error!("Failed to Parse Account {:?}, gave error {:?}, Retrying...", &data, e);
+            serde_json::from_str(&data).unwrap_or_else(|et| { 
+                fatal!("Retry failed with error {:?}", et);
+                panic!();
+            });
+        });
     }
-    return acc;
+    return Ok(acc);
 }
