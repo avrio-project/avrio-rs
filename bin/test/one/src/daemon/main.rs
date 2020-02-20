65c3659bdea46a4a3be3f476690c756c35433c85
Leo Cornelius <leocornelius019@gmail.com>
2020-02-20 16:34:26 +0000

    [TESTNETONE] Move to log

--- bin/test/one/src/daemon/main.rs
+++ bin/test/one/src/daemon/main.rs
@@ -5,10 +5,12 @@
 
 //pub extern crate config;
 pub extern crate core;
-pub extern crate crypto;
+pub extern crate config;
 pub extern crate p2p;
 pub extern crate blockchain;
 pub extern crate database;
+extern crate simple_logger;
+extern crate log;
 use libp2p::{Multiaddr, Transport, tcp::TcpConfig};
 
 fn connectSeednodes(seednodes: Vec<IpAddr::V4>) -> u8 {
@@ -17,39 +19,39 @@ fn connectSeednodes(seednodes: Vec<IpAddr::V4>) -> u8 {
     while i < seednodes.iter.count() - 1 {
         let mut error: p2p_error = p2p::connect(seednodes[i]);
         match error {
-            Err(p2p_errors::none) =>  { println("[INFO] Connected and handshaked to {:?}::{:?}", seednode[i], 11523); conn_count += 1; },
-            _ => println!("[WARN] Failed to connect to {:?}:: {:?}, returned error {:?}", seednode[i], 11523, error),
+            Err(p2p_errors::none) =>  { info!("Connected and handshaked to {:?}::{:?}", seednode[i], 11523); conn_count += 1; },
+            _ => warn!("Failed to connect to {:?}:: {:?}, returned error {:?}", seednode[i], 11523, error),
         };
         i += 1;
     }
     return conn_count;
 }
 fn existingStartup() -> u8 {
-    println!("[INFO] First startup detected, creating file structure");
+    info!("First startup detected, creating file structure");
     let mut state = database::createFileStructure();
     if state != Err(databaseError::none) {
-        println!("[ERROR] Failed to create  filestructure, recieved error {:?}.  (Fatal) Try checking permissions.", state);
+        error!("Failed to create  filestructure, recieved error {:?}.  (Fatal) Try checking permissions.", state);
         panic!(); // Faling to create the file structure is fatal but probaly just a permisions error 
     } else {
-        println!("[INFO] Succsesfuly created filestructure");
+        info!("Succsesfuly created filestructure");
     }
     drop(state);
-    println!("[INFO] Creating Chain for self");
+    info!("Creating Chain for self");
     let chainKey = core::generateChain();
     database::saveChain(chainKey);
     match chainKey[0] {
-        "0" =>  { println!("[ERROR failed to create chain (Fatal)"); panic!();},
-        _ => println("[INFO] Succsessfully created chain with chain key {}", chainKey[0]),
+        "0" =>  { error!("failed to create chain (Fatal)"); panic!();},
+        _ => info!("Succsessfully created chain with chain key {}", chainKey[0]),
     }
     let genesis_block: Block = core::generateGenesisBlock(chainKey[0], chainKey[1]);
     match blockchain::check_block(genesis_block) {
-        false => { println!("[ERROR] Failed to create genesis block, block dump: {:?} (Fatal)", genesis_block); panic!();},
-        _ => println!("[INFO] Succsessfully generated genesis block with hash {}", genesis_block.hash.to_owned()),
+        false => { error!("Failed to create genesis block, block dump: {:?} (Fatal)", genesis_block); panic!();},
+        _ => info!("Succsessfully generated genesis block with hash {}", genesis_block.hash.to_owned()),
     }
-    println!("[INFO] Launching P2p server on 127.0.0.1::{:?}", 11523); // Parsing config and having custom p2p ports to be added in 1.1.0 - 2.0.0
+    info!(" Launching P2p server on 127.0.0.1::{:?}", 11523); // Parsing config and having custom p2p ports to be added in 1.1.0 - 2.0.0
     match p2p::launchServer(11523) {
-        0 => { println!("[ERROR] Error launching P2p server on 127.0.0.1::{:?} (Fatal)", 11523); panic!();},
-        1 => println!("[INFO] Launched P2p server on 127.0.0.1::{:?}" 11523),
+        0 => { error!("Error launching P2p server on 127.0.0.1::{:?} (Fatal)", 11523); panic!();},
+        1 => info!("Launched P2p server on 127.0.0.1::{:?}" 11523),
     }
     let mut peerlist: Vec<Multiaddr>;
     let seednodes: Vec<Multiaddr> = vec![
@@ -59,22 +61,23 @@ fn existingStartup() -> u8 {
     ];
     let mut conn_nodes =0;
     while (conn_nodes < 1) {
-        println!("[WARN] Failed to connect to any seednodes, retrying");
+        warn!("Failed to connect to any seednodes, retrying");
         conn_nodes = connectSeednodes(seednodes);
     }
-    println("[INFO] Connected to seednode(s), polling for peerlist (this may take some time)");
+    info!("Connected to seednode(s), polling for peerlist (this may take some time)");
     drop(state);
     peerlist = p2p::getPeerList();
     conn_nodes += connectSeednodes(peerlist);
-    println!("[INFO] Started syncing");
+    info!("Started syncing");
     let mut sync = p2p::sync();
-    println!("[INFO] Generating Node Cerificate (for self)");
+    info!("Generating Node Cerificate (for self)");
     // generate node certificate
-    println!("[INFO] Registering with network");
+    info!("Registering with network");
     return 1;
 }
 
 fn main() {
+    simple_logger::init_with_level(log::Level::Info).unwrap();
     let art: String = "
    #    #     # ######  ### #######
   # #   #     # #     #  #  #     #
@@ -84,20 +87,20 @@ fn main() {
 #     #   # #   #    #   #  #     #
 #     #    #    #     # ### ####### ";
     println!("{}", art);
-    println!("Avrio Daemon Testnet v1.0.0 (pre-alpha)");
-    println!("[INFO] Checking for previous startup");
+    info!("Avrio Daemon Testnet v1.0.0 (pre-alpha)");
+    info!("Checking for previous startup");
     let startup_state: u16 = match database::new_startup() {
         true => existingStartup(),
         false => noExistingStartup(),
     };
     if startup_state == 1 { // succsess
-        println!("[INFO] Avrio Daemon succsessfully launched");
+        info!("Avrio Daemon succsessfully launched");
         match p2p::sync_needed() { // do we need to sync
             true => { p2p::sync(); println("[INFO] Successfully synced with the network!"); synced = true;},
             false => { println("[INFO] Succsessfully synced with the network!"); synced = true;},
         }
     }else {
-        println!("[ERROR] Failed to start avrio daemon (Fatal)");
+        error!("Failed to start avrio daemon (Fatal)");
         panic!();
     }
     if p2p::sync_needed == false // check in case a new block has been released since we syned 
@@ -128,8 +131,8 @@ fn main() {
                 new_block_s = hex::encode(new_block_s);
                 let state = p2p::send_to_all(&new_block_s);
                 if state != Err(p2pError::none) { // there was an error
-                    println("[ERROR] Failed to propiagte block {:?}, encountered error: {:?}", new_block_s, state); // tell them the error
-                    println!("[ERROR] Block dump: non serilised {:?}, hex encoded serilised {:?}, hex decoded serilised {:?}", new_block, new_block_s, hex::decode(new_block_s)); // now flood their eyes with hex 
+                    error!("Failed to propiagte block {:?}, encountered error: {:?}", new_block_s, state); // tell them the error
+                    error!("Block dump: non serilised {:?}, hex encoded serilised {:?}, hex decoded serilised {:?}", new_block, new_block_s, hex::decode(new_block_s)); // now flood their eyes with hex 
                 }
             }
         }
