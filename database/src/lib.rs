e83821b4ab9a43189ed200cb1ab5aed106ceb4df
Leo Cornelius <leocornelius019@gmail.com>
2020-02-22 23:29:46 +0000

    [DATABASE] Fix

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,35 +1,47 @@
 use rocksdb::{Options, DB};
 use serde::{Deserialize, Serialize};
-extern crate config;
+extern crate avrio_config;
+use std::process;
+#[macro_use]
 extern crate log;
-
-fn saveData(serialized: String, path: String, key: String) -> u8 {
+#[derive(Debug, Serialize, Deserialize)]
+struct PeerlistSave {
+    peers: Vec<String>,
+}
+pub fn saveData(serialized: String, path: String, key: String) -> u8 {
     // used to save data without having to create 1000's of functions (eg saveblock, savepeerlist, ect)
-    let db = DB::open_default(path).unwrap_or_else(|e| { 
-        fatal!("Failed to open database at path {}, gave error {:?}", path, e);
+    let db = DB::open_default(&path.to_owned()).unwrap_or_else(|e| { 
+        error!("Failed to open database at path {}, gave error {:?}", path, e);
+        process::exit(0);
     });
     db.put(key, serialized);
     return 1;
 }
 
-fn savePeerlist(list: &Vec<Vec<u8>>, path) {
+pub fn savePeerlist(list: &Vec<Vec<u8>>, path: String) {
     let peerlist_s: String;
     let ips: Vec<String>;
-    let ip_curr: String;
+    let mut ip_curr: String = "".to_string();
+    let mut ip_prv: String;
+    let mut pl: PeerlistSave = PeerlistSave {
+        peers: vec![]
+    };
     for ip in list {
         for ip_seg in ip {
-            ip_curr = &ip_curr + ".".to_owned() + &String::from(ip_seg);
+            ip_prv = ip_curr.clone();
+            ip_curr = ip_prv + &".".to_owned() + &ip_seg.to_string();
         }
-        ips.push(String:from(ip_curr));
+        pl.peers.push(String::from(&ip_curr));
     }
-    saveData(ips, path, "peerlist");
+    let pl_s: String = serde_json::to_string(&pl).unwrap_or_else(|e| { warn!("Failed to compact peerlist to string. Gave error: {}", e); return "".to_string()});
+    saveData(pl_s, path, "peerlist".to_string());
 }
 
-fn getData(path: String, key: String) -> String {
+pub fn getData(path: String, key: String) -> String {
     let db = DB::open_default(path).unwrap();
     let mut data: String;
     match db.get(key) {
-        Ok(Some(value)) => data = value,
+        Ok(Some(value)) => data = value.to_utf8().unwrap().to_string(),
         Ok(None) => data = "-1".to_owned(),
         Err(e) => data = "0".to_owned(),
     }
