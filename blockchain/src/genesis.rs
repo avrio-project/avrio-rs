413c8631ce0770a54637b291df245ae0412f96a4
Leo Cornelius <leocornelius019@gmail.com>
2020-02-23 14:34:37 +0000

    [BLOCKCHAIN] Make compile

--- blockchain/src/genesis.rs
+++ blockchain/src/genesis.rs
@@ -1,11 +1,22 @@
 // This file generates the genesis block for a new network.
 
 extern crate avrio_config;
+use avrio_config::config;
+extern crate avrio_database;
 use avrio_database::{getData, saveData};
 use rand::Rng;
 extern crate hex;
-
-static genesis_txns: Vec<Transactions> = vec![
+use crate::{Block, Header};
+use avrio_core::transaction::{Transaction};
+use serde::{Serialize, Deserialize};
+#[derive(Debug, PartialEq)]
+pub enum geneisBlockErrors {
+    BlockNotFound,
+    OtherDb,
+    Other,
+}
+pub fn getGenesisTxns() -> Vec<Transaction> {
+    return vec![
         // any txns to be in the genesis block are defined here, below is a template for one.
         Transaction {
             hash: String::from(""),
@@ -15,22 +26,21 @@ static genesis_txns: Vec<Transactions> = vec![
             sender_key: hex::encode(vec![0, 32]).to_owned(),
             receive_key: String::from(""),
             access_key: String::from(""),
-            gas_price: 1,
+            gas_price: 0,
             gas: 0,
             max_gas: u64::max_value(),
             nonce: 0,
             signature: String::from(""),
         },
     ];
+}
 
-fn generateGenesisBlock(chainKey: String, private_key: String) -> Block {
+pub fn generateGenesisBlock(chainKey: String) -> Block {
 
-    let mut my_genesis_txns: Vec<Transactions> = vec![];
-
+    let mut my_genesis_txns: Vec<Transaction> = vec![];
+    let genesis_txns = getGenesisTxns();
     for tx in genesis_txns {
         if tx.receive_key == chainKey {
-            tx.hash = tx.hash();
-            tx.signature = sign(hex::encode(private_key, tx.hash));
             my_genesis_txns.push(tx);
         }
     }
@@ -38,16 +48,40 @@ fn generateGenesisBlock(chainKey: String, private_key: String) -> Block {
     let mut genesis_block = Block {
         header: Header {
             version_major: 0,
-            version_minor: 0,
+            version_breaking: 0,
+            version_minor: 1,
             chain_key: chainKey,
             prev_hash: hex::encode(vec![0, 32]).to_owned(),
             height: 0,
             timestamp: 0,
         },
+        hash: "".to_string(),
         txns: my_genesis_txns,
+        signature: "".to_string(),
+        node_signatures: vec!["".to_string(); 11],
     };
 
-    genesis_block.hash = genesis_block.hash();
-    genesis_block.signature = genesis_block.sign(private_key, genesis_block.hash);
+    genesis_block.hash();
     return genesis_block;
+}
+
+pub fn getGenesisBlock(chainkey: &String) -> Result<Block, geneisBlockErrors> {
+    let data = getData(config().db_path + &"genesis-blocks".to_string(), chainkey.to_owned());
+    let none = String::from("-1");
+    let zero = String::from("0");
+    return match data {
+        none => Err(geneisBlockErrors::BlockNotFound),
+        zero => Err(geneisBlockErrors::OtherDb),
+        _  => {
+            let b: Block = serde_json::from_str(&data).unwrap_or_else(|e| {
+                warn!("Failed to parse genesis block from blob {}, gave error: {}", &data, e);
+                return Block::default();
+            });
+            if b == Block::default() {
+                return Err(geneisBlockErrors::Other);
+            } else {
+                return Ok(b);
+            }
+        },
+    };
 }
--- blockchain/src/lib.rs
+++ blockchain/src/lib.rs
@@ -1,53 +1,60 @@
 extern crate avrio_config;
 extern crate avrio_core;
-use avrio_core::{TxStore, generateGenesisBlock};
+use crate::genesis::getGenesisBlock;
+use avrio_core::transaction::*;
 use serde::{Deserialize, Serialize};
+#[macro_use]
+extern crate log;
 
-enum blockValidationErrors {
+pub enum blockValidationErrors {
     invalidBlockhash,
     badSignature,
     indexMissmatch,
     invalidPreviousBlockhash,
     invalidTransaction,
     genesisBlockMissmatch,
+    failedToGetGenesisBlock,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
 pub struct Header {
-    version_major: u8,
-    version_brekaing: u8,
-    version_minor: u8,
-    chain_key: String,
-    prev_hash: String,
-    height: u64,
-    timestamp: u64,
+    pub version_major: u8,
+    pub version_breaking: u8,
+    pub version_minor: u8,
+    pub chain_key: String,
+    pub prev_hash: String,
+    pub height: u64,
+    pub timestamp: u64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
 pub struct Block {
-    header: Header,
-    txns: Vec<TxStore>,
-    hash: String,
-    signature: String,
-    node_signatures: Vec<String>, // a block must be signed by at least (c / 2) + 1 nodes to be valid (ensures at least ne honest node has singed it)
+    pub header: Header,
+    pub txns: Vec<Transaction>,
+    pub hash: String,
+    pub signature: String,
+    pub node_signatures: Vec<String>, // a block must be signed by at least (c / 2) + 1 nodes to be valid (ensures at least ne honest node has singed it)
+}
+
+pub fn getBlock(chainkey: &String, height: u64) -> Block {
+    return Block::default();
 }
 
 impl Header {
     fn bytes(&self) -> Vec<u8> {
         let mut bytes = vec![];
 
-        bytes.extend(self.version_major.to_string());
-        bytes.extend(self.version_breaking.to_string());
-        bytes.extend(self.version_minor.to_string());
-        bytes.extend(self.chain_key);
-        bytes.extend(self.prev_hash);
-        bytes.extend(self.receive_key);
-        bytes.extend(self.height.to_string());
-        bytes.extend(self.timestamp.to_string());
+        bytes.extend(self.version_major.to_string().as_bytes());
+        bytes.extend(self.version_breaking.to_string().as_bytes());
+        bytes.extend(self.version_minor.to_string().as_bytes());
+        bytes.extend(self.chain_key.as_bytes());
+        bytes.extend(self.prev_hash.as_bytes());
+        bytes.extend(self.height.to_string().as_bytes());
+        bytes.extend(self.timestamp.to_string().as_bytes());
         bytes
     }
-    fn hash(&self) {
-        //TODO
+    fn hash(&mut self) {
+        // TODO
     }
 }
 
@@ -56,15 +63,19 @@ impl Block {
         let mut bytes = vec![];
 
         bytes.extend(self.header.bytes());
-        bytes.extend(
-            self.txns.hash
-                .iter()
-                .collect::<Vec<u8>>(),
-        );
+        for tx in self.txns.clone() {
+            bytes.extend(tx.hash.as_bytes());
+        }
         bytes
     }
-    fn hash(&self) {
+    fn hash(&mut self) {
         // TODO
+    }
+    fn sign(&mut self) {
+        //
+    }
+    fn isOtherBlock(&self, OtherBlock: &Block) -> bool {
+        self == OtherBlock
     }
 }
 
@@ -72,28 +83,32 @@ pub fn check_block(blk: Block) -> Result<(), blockValidationErrors> {
     if blk.header.height == 0 {
         // genesis block
         // could be costly
-        if blk != generateGenesisBlock() {
-            return Err(blockValidationErrors::genesisBlockMissmatch);
+        let genesis: Block;
+        match getGenesisBlock(&blk.header.chain_key) {
+            Ok(b) => genesis = b,
+            Err(e) => {
+                warn!("Failed to get genesis block for chain: {}, gave error: {:?}",&blk.header.chain_key, e);
+                return Err(blockValidationErrors::failedToGetGenesisBlock);
+            }
         }
-        else {
+        if blk != genesis {
+            return Err(blockValidationErrors::genesisBlockMissmatch);
+        } else {
             return Ok(());
         }
     } else {
         // not genesis block
-        if blk.header.prev_hash != get_block(&blk.header.chain_key, &blk.header.height - 1) {
+        if blk.header.prev_hash != getBlock(&blk.header.chain_key, &blk.header.height - 1).hash {
             return Err(blockValidationErrors::invalidPreviousBlockhash);
-        } else if check_signature(&blk.signature, hex::decode(&blk.header.chain_key.unwrap_or_else(|e| { return Err(blockValidationErrors::badSignature); }))) == Err(()) {
-            return Err(blockValidationErrors::badSignature);
-        } else {
-            for txn in blk.txns {
-                if validate_transaction(&txn) == Err(()) {
-                    return Err(blockValidationErrors::invalidTransaction);
-                } else {
-                    Ok(());
-                }
-            }
-        return Ok(());
         }
+        for txn in blk.txns {
+                    if !txn.validateTransaction() {
+                        return Err(blockValidationErrors::invalidTransaction);
+                    } else {
+                        return Ok(());
+                    }
+        }
+        return Ok(());
     }
 }
 
