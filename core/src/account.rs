26e6d5d294d77179144a1a887bdabf9ec17ee38e
Leo Cornelius <leocornelius019@gmail.com>
2020-02-28 20:10:29 +0000

    [CORE] Add Usernames

--- core/src/account.rs
+++ core/src/account.rs
@@ -1,5 +1,5 @@
 extern crate avrio_database;
-use avrio_database::{saveData, getData};
+use avrio_database::{getData, saveData};
 use serde::{Deserialize, Serialize};
 extern crate avrio_config;
 use avrio_config::config;
@@ -16,20 +16,44 @@ pub struct Accesskey {
 pub struct Account {
     // A account is a representaion of a wallet - it includes balance, a public
     pub public_key: String, // key (which is used as a index for storing) and the list of access keys.
+    pub username: String,
     pub balance: u64,
     pub access_keys: Vec<Accesskey>,
 }
 
 impl Account {
-    fn new(publicKey: String) -> Account { // allows Account::new(publicKey)
+    pub fn save(&self) -> Result<(), ()> {
+        match setAccount(self) {
+            0 => {
+                return Err(());
+            }
+            1 => {
+                return Ok(());
+            }
+            _ => {
+                return Err(());
+            }
+        };
+    }
+    pub fn new(publicKey: String) -> Account {
+        // allows Account::new(publicKey)
         let mut acc: Account = Account {
             public_key: publicKey,
+            username: "".to_string(),
             balance: 0,
-            access_keys: vec![Accesskey { key: String::from(""), allowance: 0, code: String::from("")}],
+            access_keys: vec![Accesskey {
+                key: String::from(""),
+                allowance: 0,
+                code: String::from(""),
+            }],
         };
         return acc;
     }
-    fn addAccessCode(&mut self, permCode: &String, pubKey: &String) -> Result<(), ()> {
+    pub fn addUsername(&mut self, userName: String) -> Result<(), ()> {
+        self.username = userName;
+        return Ok(());
+    }
+    pub fn addAccessCode(&mut self, permCode: &String, pubKey: &String) -> Result<(), ()> {
         let new_acc_key: Accesskey = Accesskey {
             key: pubKey.to_owned(),
             allowance: 0,
@@ -40,17 +64,26 @@ impl Account {
     }
 }
 
-pub fn setAccount(acc: Account) -> u8 {
+pub fn getByUsername(username: String) -> Result<Account, String> {
+    let publicKey: String = getData(config().db_path + &"/usernamedb".to_string(), username);
+    if let Ok(acc) = getAccount(publicKey) {
+        return Ok(acc);
+    } else {
+        return Err("failed to read account".into());
+    }
+}
+
+pub fn setAccount(acc: &Account) -> u8 {
     let path = config().db_path + "/db/accountdb";
     let serialized: String;
     serialized = serde_json::to_string(&acc).unwrap_or_else(|e| {
         error!("Unable To Serilise Account, gave error {}, retrying", e);
-        return serde_json::to_string(&acc).unwrap_or_else(|et| { 
-                error!("Retry Failed with error: {}", et);
-                panic!("Failed to serilise account");
+        return serde_json::to_string(&acc).unwrap_or_else(|et| {
+            error!("Retry Failed with error: {}", et);
+            panic!("Failed to serilise account");
         });
     });
-    saveData(serialized, path, acc.public_key);
+    saveData(serialized, path, acc.public_key.clone());
     return 1;
 }
 
@@ -60,9 +93,12 @@ pub fn getAccount(public_key: String) -> Result<Account, u8> {
     if data != "1" {
         return Err(1);
     } else {
-        let acc: Account = serde_json::from_str(&data).unwrap_or_else(|e| { 
-            error!("Failed to Parse Account {:?}, gave error {:?}, Retrying...", &data, e);
-            return serde_json::from_str(&data).unwrap_or_else(|et| { 
+        let acc: Account = serde_json::from_str(&data).unwrap_or_else(|e| {
+            error!(
+                "Failed to Parse Account {:?}, gave error {:?}, Retrying...",
+                &data, e
+            );
+            return serde_json::from_str(&data).unwrap_or_else(|et| {
                 error!("Retry failed with error {:?}", et);
                 panic!();
             });
@@ -71,8 +107,19 @@ pub fn getAccount(public_key: String) -> Result<Account, u8> {
     }
 }
 
-pub fn deltaFunds(public_key: String, amount: u64, mode: u8, access_key: String) -> Result<(), String> {
-    let mut acc: Account = getAccount(public_key.to_owned()).unwrap_or_else(|e| { error!("failed to get account with public keey {}, gave error {}", public_key, e); return Account::default();});
+pub fn deltaFunds(
+    public_key: String,
+    amount: u64,
+    mode: u8,
+    access_key: String,
+) -> Result<(), String> {
+    let mut acc: Account = getAccount(public_key.to_owned()).unwrap_or_else(|e| {
+        error!(
+            "failed to get account with public keey {}, gave error {}",
+            public_key, e
+        );
+        return Account::default();
+    });
     if mode == 0 {
         // minus funds
         if access_key == "" {
@@ -80,11 +127,16 @@ pub fn deltaFunds(public_key: String, amount: u64, mode: u8, access_key: String)
             let after_change = acc.balance - amount;
             if after_change < 0 {
                 // insufffient funds
-                warn!("changing funds for account {} would produce negative balance!", acc.public_key);
-                return Err("changing funds for account {} would produce negative balance".to_string());
+                warn!(
+                    "changing funds for account {} would produce negative balance!",
+                    acc.public_key
+                );
+                return Err(
+                    "changing funds for account {} would produce negative balance".to_string(),
+                );
             } else {
                 acc.balance = acc.balance - amount;
-                return match setAccount(acc) {
+                return match setAccount(&acc) {
                     1 => Ok(()),
                     _ => Err("failed to set account".to_string()),
                 };
@@ -111,7 +163,7 @@ pub fn deltaFunds(public_key: String, amount: u64, mode: u8, access_key: String)
                 } else {
                     acc.balance = acc.balance - amount;
                     acc.access_keys[i].allowance = acc.access_keys[i].allowance - amount;
-                    return match setAccount(acc) {
+                    return match setAccount(&acc) {
                         1 => Ok(()),
                         _ => Err("Failed to save account".to_string()),
                     };
@@ -123,7 +175,7 @@ pub fn deltaFunds(public_key: String, amount: u64, mode: u8, access_key: String)
         if access_key == "" {
             // none provdied/ using main key
             acc.balance = acc.balance + amount;
-            return match setAccount(acc) {
+            return match setAccount(&acc) {
                 1 => Ok(()),
                 _ => Err("Failed to save account".to_string()),
             };
@@ -142,10 +194,10 @@ pub fn deltaFunds(public_key: String, amount: u64, mode: u8, access_key: String)
             } else {
                 acc.access_keys[i].allowance = acc.access_keys[i].allowance + amount;
                 acc.balance = acc.balance + amount;
-                return match setAccount(acc) {
+                return match setAccount(&acc) {
                     1 => Ok(()),
                     _ => Err("Failed to save account".to_string()),
-                }
+                };
             }
         }
     }
