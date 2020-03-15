63f0d7285df81ab16aea538cd9242e1efd62cca5
Leo Cornelius <leocornelius019@gmail.com>
2020-03-15 16:50:45 +0000

    [RPC] Begin RPC code

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,1 +1,17 @@
+/*
+Copyright The Avrio Core Developers 2020
+rpc/lib.rs - 
+  this file handles the json api
+*/
 
+#![feature(proc_macro_hygiene, decl_macro)]
+use serde_json::json;
+#[macro_use] extern crate rocket;
+#[get("/")]
+fn must_provide_method() -> &'static str {
+    "{ \"response\": 400, \"error\": \"Must provide a method\" }"
+}
+
+pub fn start_server() {
+    rocket::ignite().mount("/json_rpc", routes![must_provide_method]).launch();
+}
