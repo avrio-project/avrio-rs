660839aff81f9d73d4a34b4826c14dd62720ea95
Leo Cornelius <leocornelius019@gmail.com>
2020-03-15 16:50:32 +0000

    [TOOLS] Add buildclang.sh

---
+++ buildclang.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install aptitude -y
+sudo aptitude install -y -o Aptitude::ProblemResolver::SolutionCost='100*canceled-actions,200*removals' build-essential clang-6.0 libstdc++-7-dev git libboost-all-dev python-pip libssl1.0-dev
+sudo pip install cmake
