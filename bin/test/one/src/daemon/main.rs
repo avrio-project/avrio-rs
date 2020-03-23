771fc31fc02740d2e6cdd75e30e633a708805fee
Leo Cornelius <leocornelius019@gmail.com>
2020-03-23 19:10:28 +0000

    [TESTNET ONE] Fix daemon bins

--- bin/test/one/src/daemon/main.rs
+++ bin/test/one/src/daemon/main.rs
@@ -6,13 +6,17 @@
 extern crate clap;
 use clap::App;
 
+use std::fs::create_dir_all;
+
+use serde_json::*;
+
 extern crate ring;
 use ring::{
     rand as randc,
     signature::{self, KeyPair},
 };
 
-use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
 use std::process;
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -31,52 +35,66 @@ extern crate avrio_p2p;
 use avrio_p2p::{new_connection, prop_block, rec_server, sync, sync_needed};
 
 extern crate avrio_blockchain;
-use avrio_blockchain::*;
+use avrio_blockchain::{genesis::*, *};
 
 extern crate avrio_database;
-use avrio_database::{getData, saveData, savePeerlist};
+use avrio_database::{getData, getPeerList, saveData, savePeerlist};
 
 #[macro_use]
 extern crate log;
 extern crate simple_logger;
 
+fn generateKeypair(out: &mut Vec<String>) {
+    out.push("".to_owned());
+    out.push("".to_owned());
+}
+
 fn database_present() -> bool {
-    let get_res =  getData(config().db_path + &"/chainsindex".to_owned(), "digest".to_string());
+    let get_res = getData(
+        config().db_path + &"/chainsindex".to_owned(),
+        &"digest".to_owned(),
+    );
     if get_res == "-1".to_owned() {
         return false;
     } else if get_res == "0".to_string() {
-        return false
+        return false;
     } else {
         return true;
     }
 }
 
-fn createFileStructure() -> Result<(), std::error::Error> {
-    fs::create_dir_all(config().db_path + &"/blocks".to_string())?;
-    fs::create_dir_all(config().db_path + &"/chains".to_string())?;
+fn createFileStructure() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    create_dir_all(config().db_path + &"/blocks".to_string())?;
+    create_dir_all(config().db_path + &"/chains".to_string())?;
     return Ok(());
 }
 
-fn connectSeednodes(seednodes: Vec<SocketAddr>, connected_peers: Vec<SocketAddr>) -> u8 {
+fn connectSeednodes(seednodes: Vec<SocketAddr>, connected_peers: &mut Vec<TcpStream>) -> u8 {
     let mut i: usize = 0;
     let mut conn_count: u8 = 0;
-    while i < seednodes.iter.count() - 1 {
-        let mut error: p2p_error = new_connection(seednodes[i]);
+    while i < seednodes.iter().count() - 1 {
+        let mut error = new_connection(seednodes[i]);
         match error {
             Ok(_) => {
-                info!("Connected and handshaked to {:?}::{:?}", seednode[i], 11523);
+                info!(
+                    "Connected and handshaked to {:?}::{:?}",
+                    seednodes[i], 11523
+                );
                 conn_count += 1;
+                let mut peer = error.unwrap();
+                let mut peer_cloned = peer.stream.try_clone().unwrap();
+                connected_peers.push(peer_cloned);
             }
             _ => warn!(
                 "Failed to connect to {:?}:: {:?}, returned error {:?}",
-                seednode[i], 11523, error
+                seednodes[i], 11523, error
             ),
         };
         i += 1;
     }
     return conn_count;
 }
-fn firstStartUp() -> u8 {
+fn firstStartUp() -> u16 {
     info!("First startup detected, creating file structure");
     let mut state = createFileStructure();
     if let Err(e) = state {
@@ -87,21 +105,20 @@ fn firstStartUp() -> u8 {
     }
     drop(state);
     info!("Creating Chain for self");
-    let mut chainKey = ["".to_String(); 2]; // 0 = pubkey, 1 = privkey
+    let mut chainKey: Vec<String> = vec![]; // 0 = pubkey, 1 = privkey
     generateKeypair(&mut chainKey);
-    match chainKey[0] {
-        "0" => {
-            error!("failed to create keypair (Fatal)");
-            process::exit(1);
-        }
-        _ => info!(
+    if chainKey[0] == "0".to_owned() {
+        error!("failed to create keypair (Fatal)");
+        process::exit(1);
+    } else {
+        info!(
             "Succsessfully created keypair with chain key {}",
             chainKey[0]
-        ),
+        );
     }
-    let mut genesis_block = avrio_core::getGenesisBlock(&chainKey[0]);
+    let mut genesis_block = getGenesisBlock(&chainKey[0]);
     if let Err(e) = genesis_block {
-        if e == geneisBlockErrors::BlockNotFound {
+        if e == genesisBlockErrors::BlockNotFound {
             info!(
                 "No genesis block found for chain: {}, generating",
                 chainKey[0]
@@ -115,23 +132,25 @@ fn firstStartUp() -> u8 {
             process::exit(1);
         }
     }
+    let genesis_block = genesis_block.unwrap();
+    let genesis_block_clone = genesis_block.clone();
     match check_block(genesis_block) {
         Err(error) => {
             error!(
                 "Failed to create/ get genesis block. Gave error: {:?}, block dump: {:?}. (Fatal)",
-                error, genesis_block
+                error, genesis_block_clone
             );
             process::exit(1);
         }
-        _ => info!(
+        Ok(_) => info!(
             "Succsessfully generated/got genesis block with hash {}",
-            genesis_block.hash
+            genesis_block_clone.hash
         ),
     }
     let e_code = saveData(
-        serde_json::to_string(&genesis_block.unwrap()),
-        config().db_path + &"/".to_string() + chainKey[0],
-        genesis_block.hash.clone(),
+        serde_json::to_string(&genesis_block_clone).unwrap(),
+        config().db_path + &"/".to_string() + &chainKey[0],
+        genesis_block_clone.hash.clone(),
     );
     match e_code {
         1 => {
@@ -141,7 +160,7 @@ fn firstStartUp() -> u8 {
         _ => {
             error!(
                 "Failed to save genesis block with hash: {}, gave error code: {} (fatal)!",
-                genesis_block.hash, e_code
+                genesis_block_clone.hash, e_code
             );
             process::exit(1);
         }
@@ -157,30 +176,37 @@ fn firstStartUp() -> u8 {
             process::exit(1);
         }
     }
-    let mut peerlist: Vec<SocketAddr>;
-    let seednodes: Vec<SockerAddr> = vec![
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12345),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12345),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12345),
-    ];
+    let peerlist: Vec<SocketAddr>;
     let mut conn_nodes = 0;
-    let trys: u8 = 0;
-    while (conn_nodes < 1) {
+    let mut trys: u8 = 0;
+    let mut connected_peers: Vec<TcpStream> = vec![];
+    while conn_nodes < 1 {
+        let seednodes: Vec<SocketAddr> = vec![
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12345),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12345),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12345),
+        ];
         if trys > 49 {
             process::exit(1);
         }
         if trys > 0 {
             warn!("Failed to connect to any seednodes, retrying");
         }
-        conn_nodes = connectSeednodes(seednodes);
+        conn_nodes = connectSeednodes(seednodes, &mut connected_peers);
         trys += 1;
     }
     info!("Connected to seednode(s), polling for peerlist (this may take some time)");
-    drop(state);
-    peerlist = getPeerList();
-    conn_nodes += connectSeednodes(peerlist, connected_peers);
+    peerlist = getPeerList().unwrap();
+    conn_nodes += connectSeednodes(peerlist, &mut connected_peers);
     info!("Started syncing");
-    let mut sync = sync(connected_peers);
+    let con_peer_len = connected_peers.len();
+    for mut peer_val in connected_peers {
+        let mut connected_peers_mut: Vec<&mut TcpStream> = vec![];
+        connected_peers_mut.push(&mut peer_val);
+        if connected_peers_mut.len() == con_peer_len {
+            let sync = sync(&mut connected_peers_mut);
+        }
+    }
     info!("Generating Node Cerificate (for self)");
     // TODO: generate node certificate
     info!("Registering with network");
@@ -188,19 +214,26 @@ fn firstStartUp() -> u8 {
     return 1;
 }
 
-fn send_block() {
+fn send_block(chainKey: String, height: u64, private_key: String) {
     // create block
-    let transactions: Vec<transactions> = vec![
+    let transactions: Vec<Transaction> = vec![
         //todo
     ];
-    let new_block = Block {
+    let prev_hash = "00000000".to_owned();
+    if height != 0 {
+        getData(
+            config().db_path + &"/".to_owned() + &chainKey + &"-invs".to_owned(),
+            &(height - 1).to_string(),
+        );
+    }
+    let mut new_block = Block {
         header: Header {
             version_major: 0,
             version_breaking: 0,
             version_minor: 0,
-            chain_key: chainKey[0],
-            prev_hash: getHashForHeight(height - 1),
-            height: 1,
+            chain_key: chainKey.clone(),
+            prev_hash,
+            height,
             timestamp: SystemTime::now()
                 .duration_since(UNIX_EPOCH)
                 .expect("Time went backwards")
@@ -209,29 +242,29 @@ fn send_block() {
         txns: transactions,
         hash: String::from(""),
         signature: String::from(""),
-        nonce: 1234,
+        nonce: "1234".to_owned(),
         node_signatures: vec![],
     };
-    new_block.hash = new_block.hash();
-    new_block.signature = new_block.sign(private_key);
+    new_block.hash();
+    new_block.sign(&private_key);
     let mut new_block_s: String = "".to_string();
-    if check_block(new_block) {
-        new_block_s = serde_json::to_string(&new_block).unwrap();
-        new_block_s = new_block_s;
-        let state = prop_block(&new_block_s);
-        if state != Err(p2pError::none) {
+    if let Ok(_) = check_block(new_block.clone()) {
+        let state = prop_block(new_block.clone());
+        if let Err(e) = state {
             // there was an error
             error!(
                 "Failed to propiagte block {:?}, encountered error: {:?}",
-                new_block_s.hash, state
+                new_block.hash, e
             ); // tell them the error
-            error!(
-                "Block dump: non serilised {:?}, serilised {}",
-                new_block, new_block_s
-            ); // now flood their eyes
+            error!("Block dump: non serilised {:?}", new_block); // now flood their eyes
             process::exit(1);
         }
     }
+}
+
+// TODO: write existing start up code
+fn existingStartup() -> u16 {
+    return 1;
 }
 
 fn main() {
@@ -251,16 +284,24 @@ fn main() {
         true => existingStartup(),
         false => firstStartUp(),
     };
-    let synced: bool = false;
+    let mut synced: bool = false;
     if startup_state == 1 {
         // succsess
         info!("Avrio Daemon succsessfully launched");
         match sync_needed() {
             // do we need to sync
             true => {
-                sync();
-                info!("Successfully synced with the network!");
-                synced = true;
+                let pl: Vec<SocketAddr> = getPeerList().unwrap();
+                for peer in pl {
+                    let res = new_connection(peer);
+                    if let Ok(mut peer_struct) = res {
+                        let mut peers: Vec<&mut TcpStream> = vec![];
+                        peers.push(&mut peer_struct.stream);
+                        sync(&mut peers);
+                        info!("Successfully synced with the network!");
+                        synced = true;
+                    }
+                }
             }
             false => {
                 info!("Succsessfully synced with the network!");
@@ -271,22 +312,57 @@ fn main() {
         error!("Failed to start avrio daemon (Fatal)");
         panic!();
     }
-    if sync_needed == false
+    if sync_needed() == false
     // check in case a new block has been released since we syned
     {
-        send_block();
+        // TODO: !!!URGENT!!!, use encryption on the wallet!!!
+        let pubkey = getData(
+            config().db_path + &"/wallets/wallet".to_owned(),
+            &"pubkey".to_owned(),
+        );
+        let privkey = getData(
+            config().db_path + &"/wallets/wallet".to_owned(),
+            &"privkey".to_owned(),
+        );
+        send_block(pubkey, 0, privkey);
     } else {
         match sync_needed() {
             // do we need to sync
             true => {
-                sync();
-                info!("Successfully synced with the network!");
-                synced = true;
-                send_block();
+                let pl: Vec<SocketAddr> = getPeerList().unwrap();
+                for peer in pl {
+                    let res = new_connection(peer);
+                    if let Ok(mut peer_struct) = res {
+                        let mut peers: Vec<&mut TcpStream> = vec![];
+                        peers.push(&mut peer_struct.stream);
+                        sync(&mut peers);
+                        info!("Successfully synced with the network!");
+                        synced = true;
+                    }
+                }
+                // TODO: !!!URGENT!!!, use encryption on the wallet!!!
+                let pubkey = getData(
+                    config().db_path + &"/wallets/wallet".to_owned(),
+                    &"pubkey".to_owned(),
+                );
+                let privkey = getData(
+                    config().db_path + &"/wallets/wallet".to_owned(),
+                    &"privkey".to_owned(),
+                );
+                send_block(pubkey, 0, privkey);
             }
             false => {
                 synced = true;
-                send_block();
+                // TODO: !!!URGENT!!!, use encryption on the wallet!!!
+                let pubkey = getData(
+                    config().db_path + &"/wallets/wallet".to_owned(),
+                    &"pubkey".to_owned(),
+                );
+                let privkey = getData(
+                    config().db_path + &"/wallets/wallet".to_owned(),
+                    &"privkey".to_owned(),
+                );
+                send_block(pubkey, 0, privkey);
             }
         };
     }
