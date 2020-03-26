1205b35c88d563657b702e4fe58b3cd0c282078d
Leo Cornelius <leocornelius019@gmail.com>
2020-03-26 19:24:32 +0000

    [CRYPTO] Add in argon2id round

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,5 +1,10 @@
+// Lyra2
 extern crate lyra2;
 use lyra2::lyra2rev3::sum;
+
+// Argon2
+use argonautica::Hasher;
+
 
 extern crate bs58;
 pub trait Hashable {
@@ -8,6 +13,19 @@ pub trait Hashable {
     fn hash_item (&self) -> String{
         let bytes = self.bytes();
         let lyra2res = sum(bytes);
-        return bs58::encode(lyra2res).into_string();
+        let mut hasher = Hasher::default()
+        .opt_out_of_secret_key(true);
+        let mut sk = 0;
+        while i < bytes.len() {
+            sk += bytes[i]
+        }
+        let hash = hasher
+        .with_password(lyra2res)
+        .with_salt(bs58::encode(sk).into_string())
+        .with_secret_key(bs58::encode(sk).into_string())
+        .with_salt(bs)
+        .hash()
+        .unwrap_or("hash".to_string());
+        return bs58::encode(hash).into_string();
     }
 }
