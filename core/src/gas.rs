d00f2e802e96f7af95c39b11fe93859f33e1bde9
Leo Cornelius <leocornelius019@gmail.com>
2020-04-09 18:35:41 +0100

    [CORE][GAS] Add brun+return gas ammoutn

--- core/src/gas.rs
+++ core/src/gas.rs
@@ -7,3 +7,4 @@ pub const TX_GAS: u8 = 20; // the gas used in a normal transaction (no extra)
 pub const GAS_PER_EXTRA_BYTE_NORMAL: u8 = 60; // fee per byte of extra data in a transaction with amount > 0 and a recipitent
 pub const GAS_PER_EXTRA_BYTE_MESSAGE: u8 = 100; // fee per byte of extra data in a message transaction (a transaction with amount = 0)
 pub const GENESIS_MAX_GAS: u64 = u64::max_value(); // The max gas for a genesis block
+pub const BURN_AND_RETURN_GAS: u64 = 5;
