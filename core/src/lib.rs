e233a00978645e7d92bf8f33e0fed980861c297a
Leo Cornelius <leocornelius019@gmail.com>
2020-04-24 13:11:50 +0100

    [CORE] Needed includes

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,12 +1,14 @@
 #[macro_use]
 extern crate log;
 
+extern crate avrio_p2p;
+
 pub fn safe_exit() {
     // TODO: save mempool to disk + send kill to all threads.
 
     info!("Goodbye!");
     avrio_p2p::close_all(connections_mut);
-    process::exit(0);
+    std::process::exit(0);
 }
 
 pub mod account;
