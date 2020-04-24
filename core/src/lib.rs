189f5cc1988dc4317fb150ae0fa7417f3a503cc3
Leo Cornelius <leocornelius019@gmail.com>
2020-04-24 13:16:57 +0100

    [CORE] Dont include avrio_p2p in core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,16 +1,6 @@
 #[macro_use]
 extern crate log;
 
-extern crate avrio_p2p;
-
-pub fn safe_exit() {
-    // TODO: save mempool to disk + send kill to all threads.
-
-    info!("Goodbye!");
-    avrio_p2p::close_all(connections_mut);
-    std::process::exit(0);
-}
-
 pub mod account;
 pub mod certificate;
 pub mod epoch;
