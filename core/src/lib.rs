1a0225d6ba87c531226c5445875f5002f0d44cbb
Leo Cornelius <leocornelius019@gmail.com>
2020-04-24 13:05:20 +0100

    [CORE] Add safe_exit function

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,14 @@
 #[macro_use]
 extern crate log;
 
+pub fn safe_exit() {
+    // TODO: save mempool to disk + send kill to all threads.
+
+    info!("Goodbye!");
+    avrio_p2p::close_all(connections_mut);
+    process::exit(0);
+}
+
 pub mod account;
 pub mod certificate;
 pub mod epoch;
