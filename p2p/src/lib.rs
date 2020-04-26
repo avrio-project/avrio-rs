8d1908c4d6e46b965aa74896a9e13fba6947b5b7
Leo Cornelius <leocornelius019@gmail.com>
2020-04-26 12:38:49 +0100

    [P2P] Use correct chain digest location

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -786,11 +786,11 @@ pub fn sync(pl: &mut Vec<&mut TcpStream>) -> Result<u64, String> {
         }
     }
     info!("Synced all chains, checking chain digest with peers");
-    if getData(config().db_path + &"/chainsdigest", "master") != mode_hash {
+    if getData(config().db_path + &"/chaindigest", "master") != mode_hash {
         error!("Synced blocks do not result in mode block hash, if you have appended blocks (using send_txn or generate etc) then ignore this. If not please delete your data ir and resync");
         error!(
             "Our CD: {}, expected: {}",
-            getData(config().db_path + &"/chainsdigest", "master"),
+            getData(config().db_path + &"/chaindigest", "master"),
             mode_hash
         );
         return Err("invalid resultant chain digest".into());
