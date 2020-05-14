fa72e1259498a3a5368c45d890cd5a83d3371d61
Leo Cornelius <leocornelius019@gmail.com>
2020-05-14 14:24:33 +0100

    [P2P] Add list of the peers syncing to static ref

--- p2p/src/handle.rs
+++ p2p/src/handle.rs
@@ -21,26 +21,55 @@ use x25519_dalek::PublicKey;
 static MAX_SYNCING_PEERS: u64 = 8;
 
 lazy_static! {
-    static ref SYNCING_PEERS: Mutex<u64> = Mutex::new(0);
+    static ref SYNCING_PEERS: Mutex<(u64, Vec<String>)> = Mutex::new((0, vec![]));
 }
 
 fn get_syncing_peers_count() -> Result<u64, Box<dyn std::error::Error>> {
-    return Ok(*SYNCING_PEERS.lock()?);
+    return Ok(SYNCING_PEERS.lock()?.0);
 }
 
 fn set_syncing_peers_count(new: u64) -> Result<(), Box<dyn std::error::Error>> {
-    *SYNCING_PEERS.lock()? = new;
+    SYNCING_PEERS.lock()?.0 = new;
     return Ok(());
 }
 
 fn increment_sync_count() -> Result<(), Box<dyn std::error::Error>> {
     let new = get_syncing_peers_count()? + 1;
-    *SYNCING_PEERS.lock()? = new;
+    SYNCING_PEERS.lock()?.0 = new;
+    return Ok(());
+}
+
+fn add_peer_to_sync_list(peer: &SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+    let peer_no_port = crate::peer::strip_port(peer);
+    if !SYNCING_PEERS.lock()?.1.iter().any(|i| i == &peer_no_port) {
+        increment_sync_count()?;
+        SYNCING_PEERS.lock()?.1.push(peer_no_port);
+    } else {
+        return Err("Peer already in syncing peers list".into());
+    }
+    return Ok(());
+}
+fn peer_syncing(peer: &SocketAddr) -> Result<bool, Box<dyn std::error::Error>> {
+    return Ok(SYNCING_PEERS
+        .lock()?
+        .1
+        .iter()
+        .any(|i| i == &crate::peer::strip_port(peer)));
+}
+
+fn remove_peer_from_sync_list(peer: &SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+    let peer_no_port = crate::peer::strip_port(peer);
+    if SYNCING_PEERS.lock()?.1.iter().any(|i| i == &peer_no_port) {
+        deincrement_sync_count()?;
+        SYNCING_PEERS.lock()?.1.retain(|x| x != &peer_no_port);
+    } else {
+        return Err("Peer not in syncing peers list".into());
+    }
     return Ok(());
 }
 
 fn deincrement_sync_count() -> Result<(), Box<dyn std::error::Error>> {
-    let mut new = {
+    let new = {
         let spc = get_syncing_peers_count()?;
         if spc == 0 {
             0
@@ -48,7 +77,7 @@ fn deincrement_sync_count() -> Result<(), Box<dyn std::error::Error>> {
             spc - 1
         }
     };
-    *SYNCING_PEERS.lock()? = new;
+    SYNCING_PEERS.lock()?.0 = new;
     return Ok(());
 }
 
@@ -90,13 +119,30 @@ pub fn launch_handle_client(
                                 let slots_left = MAX_SYNCING_PEERS - used_slots;
                                 log::trace!("Recieved sync request from peer, current syncing peers: {}, slots left: {}", used_slots, slots_left);
                                 if slots_left > 0 {
-                                    let _ = send("syncack".into(), &mut stream, 0x01, true, None);
-                                    let _ = deincrement_sync_count();
+                                    if let Ok(_) = add_peer_to_sync_list(
+                                        &stream.peer_addr().unwrap_or(SocketAddr::new(
+                                            IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                                            0,
+                                        )),
+                                    ) {
+                                        let _ =
+                                            send("syncack".into(), &mut stream, 0x01, true, None);
+                                    } else {
+                                        let _ =
+                                            send("syncdec".into(), &mut stream, 0x01, true, None);
+                                    }
                                 } else {
                                     let _ = send("syncdec".into(), &mut stream, 0x01, true, None);
                                 }
                             }
+                            0x23 => {
+                                // end syncing
+                                let _ = remove_peer_from_sync_list(&stream.peer_addr().unwrap_or(
+                                    SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0),
+                                ));
+                            }
                             0x1b | 0x1c => {
+                                // send the peer our chain digest
                                 log::trace!("Sending chain digest to peer");
                                 let chain_digest = avrio_database::getData(
                                     avrio_config::config().db_path + &"/chainsdigest",
@@ -105,12 +151,15 @@ pub fn launch_handle_client(
                                 let _ = send(chain_digest, &mut stream, 0xcd, true, None);
                             }
                             0x05 => {
+                                // send the peer the block with the hash they specifyed in the message field
                                 let _ = crate::helper::send_block_with_hash(
                                     read_msg.message,
                                     &mut stream,
                                 );
                             }
                             0x0a => {
+                                // the peer just sent us a block, 
+                                // validate it, save it an enact it
                                 let block: avrio_blockchain::Block =
                                     serde_json::from_str(&read_msg.message).unwrap_or_default();
                                 if block.is_default() {
@@ -164,6 +213,7 @@ pub fn launch_handle_client(
                                                     true,
                                                     None,
                                                 );
+                                                log::error!("");
                                             } else {
                                                 log::debug!("Recieved and processed block from peer, sending block ack");
                                                 let _ = send(
@@ -180,6 +230,7 @@ pub fn launch_handle_client(
                             }
                             0x1a => log::debug!("Got handshake from handshook peer, ignoring"),
                             _ => {
+                                log::debug!("Got unsupported message type: \"0x{:x}\", please check for updates", read_msg.message_type);
                                 ();
                             }
                         }
@@ -191,11 +242,4 @@ pub fn launch_handle_client(
         },
     );
     return Ok(());
-}
-
-fn from_slice(bytes: &[u8]) -> [u8; 32] {
-    let mut array = [0; 32];
-    let bytes = &bytes[..array.len()]; // panics if not enough data
-    array.copy_from_slice(bytes);
-    array
 }
