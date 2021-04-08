2c19e2f0471ff02991afe9e679038d9eb69a3d1f
LeoCornelius <leocornelius019@gmail.com>
2021-04-08 16:42:32 +0100

    [CORE] Transaction add new transaction validation code

--- core/src/transaction.rs
+++ core/src/transaction.rs
@@ -5,6 +5,7 @@ extern crate avrio_config;
 extern crate bs58;
 use avrio_config::config;
 extern crate rand;
+use avrio_database::{get_data, save_data};
 
 use ring::signature;
 
@@ -20,6 +21,9 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug, PartialEq)]
 pub enum TransactionValidationErrors {
+    TransactionExists,
+    CouldNotGetSenderAcc,
+    CouldNotGetRecieverAcc,
     AccountMissing,
     BadNonce,
     InsufficentBalance,
@@ -40,6 +44,7 @@ pub enum TransactionValidationErrors {
     LowGas,
     UnsupportedType,
     ExtraNotAlphanumeric,
+    WouldOverflowBalance,
     Other,
 }
 
@@ -98,6 +103,8 @@ impl Transaction {
             'm' => "message".to_string(),
             'c' => "claim".to_owned(), // This is only availble on the testnet it will be removed before the mainet
             'i' => "create invite".to_owned(),
+            'x' => "Block/ restrict account".to_owned(), // means the account (linked via public key in the extra field) you block cannot send you transactions
+            'p' => "Unblock account".to_owned(), // reverts the block transaction (linked by the txn hash in extra field)
             _ => "unknown".to_string(),
         }
     }
@@ -192,10 +199,337 @@ impl Transaction {
         self.gas * self.gas_price
     }
 
-    pub fn valid(&self, recieve: bool) -> Result<(), TransactionValidationErrors> {
+    pub fn valid(&self) -> Result<(), TransactionValidationErrors> {
+        trace!("Validating txn with hash: {}", self.hash);
+        let start = SystemTime::now();
+        let sender_account: Account;
+        if self.sender_key.len() < 44 {
+            // public keys are 44 bytes long, check if sender key is a public key or a username
+            match get_account(&self.sender_key) {
+                Ok(got_account) => sender_account = got_account,
+                Err(e) => {
+                    error!(
+                        "Failed to get sender's account by publickey: sender_key={}, error={}",
+                        self.sender_key, e
+                    );
+                    return Err(TransactionValidationErrors::CouldNotGetSenderAcc);
+                }
+            }
+        } else {
+            match get_account(&self.sender_key) {
+                Ok(got_account) => sender_account = got_account,
+                Err(e) => {
+                    error!(
+                        "Failed to get sender's account by username: sender_key={}, error={}",
+                        self.sender_key, e
+                    );
+                    return Err(TransactionValidationErrors::CouldNotGetSenderAcc);
+                }
+            }
+        }
+        if self.hash_return() != self.hash {
+            return Err(TransactionValidationErrors::BadHash);
+        }
+        let account_nonce = get_data(
+            config().db_path
+                + &"/chains/".to_owned()
+                + &self.sender_key
+                + &"-chainindex".to_owned(),
+            &"txncount".to_owned(),
+        );
+        if self.nonce.to_string() != account_nonce && account_nonce != "-1" {
+            return Err(TransactionValidationErrors::BadNonce);
+        }
+        let block_txn_is_in = get_data(config().db_path + &"/transactions".to_owned(), &self.hash);
+        if block_txn_is_in != *"-1" {
+            error!(
+                "Transaction {} already in block {}",
+                self.hash, block_txn_is_in
+            );
+            return Err(TransactionValidationErrors::TransactionExists);
+        }
+        if !['c', 'n', 'b', 'u'].contains(&self.flag) {
+            error!(
+                "Transaction {} has unsupported type={} ({})",
+                self.hash,
+                self.flag,
+                self.type_transaction()
+            );
+            return Err(TransactionValidationErrors::UnsupportedType);
+        }
+        if !self.extra.chars().all(char::is_alphanumeric) {
+            error!("Transaction {} has non alphanumeric extra field", self.hash);
+            return Err(TransactionValidationErrors::ExtraNotAlphanumeric);
+        }
+        let gas_price_min = 1; // todo move to config
+        if self.gas_price <= gas_price_min {
+            error!(
+                "Transaction {}'s gas price too low ({} < {})",
+                self.hash, self.gas_price, gas_price_min
+            );
+            return Err(TransactionValidationErrors::GasPriceLow);
+        }
+        match self.flag {
+            'n' => {
+                let size_of_extra = std::mem::size_of_val(&self.extra);
+                if size_of_extra > 100 {
+                    error!(
+                        "Normal type transaction {}'s extra ({}) too large, {} > 100",
+                        self.hash, self.extra, size_of_extra
+                    );
+                    return Err(TransactionValidationErrors::ExtraTooLarge);
+                }
+                if self.amount < 1 {
+                    error!("Transaction {} amount too small", self.hash);
+                    return Err(TransactionValidationErrors::InsufficentAmount);
+                }
+                if sender_account.balance < (self.amount + (self.gas * self.gas_price)) {
+                    error!("Sender {} of transaction {}'s balance too low, amount {} fee={} ({} * {}), required delta={}", sender_account.public_key, self.hash, self.amount, self.gas * self.gas_price, self.gas , self.gas_price, (self.amount + (self.gas * self.gas_price)) - sender_account.balance);
+                    return Err(TransactionValidationErrors::InsufficentBalance);
+                }
+                let receiver_account: Account;
+                if self.sender_key.len() < 44 {
+                    // public keys are 44 bytes long, check if receive key is a public key or a username
+                    match get_account(&self.receive_key) {
+                        Ok(got_account) => receiver_account = got_account,
+                        Err(e) => {
+                            error!(
+                                "Failed to get receiver's account by publickey: receive_key={}, error={}",
+                                self.receive_key, e
+                            );
+                            return Err(TransactionValidationErrors::CouldNotGetSenderAcc);
+                        }
+                    }
+                } else {
+                    match get_account(&self.receive_key) {
+                        Ok(got_account) => receiver_account = got_account,
+                        Err(e) => {
+                            error!(
+                                "Failed to get receiver's account by username: receive_key={}, error={}",
+                                self.receive_key, e
+                            );
+                            return Err(TransactionValidationErrors::CouldNotGetSenderAcc);
+                        }
+                    }
+                }
+                if (u64::MAX - receiver_account.balance) < self.amount {
+                    // check the reciever's balance will not surpass u64::MAX after this tranaction and overflow (this is only really a concern with the testnet which has CLAIM transactions)
+                    error!("Transaction {} would overflow receiver {}'s balance: Current={}, safe_left={}, txn_amount={}", self.hash, self.receive_key, receiver_account.balance, (u64::MAX - receiver_account.balance), self.amount);
+                    return Err(TransactionValidationErrors::WouldOverflowBalance);
+                }
+                if self.gas
+                    < (TX_GAS as u64 + (GAS_PER_EXTRA_BYTE_NORMAL as u64 * self.extra.len() as u64))
+                {
+                    error!("Transaction {} gas low", self.hash);
+                    return Err(TransactionValidationErrors::LowGas);
+                }
+                if self.max_gas < self.gas {
+                    error!(
+                        "Transaction {} max_gas expended, max_gas={}, used_gas={}",
+                        self.hash, self.max_gas, self.gas
+                    );
+                    return Err(TransactionValidationErrors::MaxGasExpended);
+                }
+                if self.unlock_time != 0 {
+                    return Err(TransactionValidationErrors::UnsupportedType); // TODO: Implement unlock time
+                }
+            }
+            'c' => {
+                let size_of_extra = std::mem::size_of_val(&self.extra);
+                if size_of_extra != 0 {
+                    error!(
+                        "Claim type transaction {}'s extra ({}) not zero, {} != 0",
+                        self.hash, self.extra, size_of_extra
+                    );
+                    return Err(TransactionValidationErrors::ExtraTooLarge);
+                }
+                if self.amount < 1 {
+                    error!("Transaction {} amount too small", self.hash);
+                    return Err(TransactionValidationErrors::InsufficentAmount);
+                }
+                if (u64::MAX - sender_account.balance) < self.amount {
+                    // check the reciever's balance will not surpass u64::MAX after this tranaction and overflow (this is only really a concern with the testnet which has CLAIM transactions)
+                    error!("Claim transaction {} would overflow sender's {}'s balance: Current={}, safe_left={}, txn_amount={}", self.hash, self.receive_key, sender_account.balance, (u64::MAX - sender_account.balance), self.amount);
+                    return Err(TransactionValidationErrors::WouldOverflowBalance);
+                }
+                if self.unlock_time != 0 {
+                    return Err(TransactionValidationErrors::UnsupportedType); // TODO: Implement unlock time
+                }
+            }
+            'u' => {
+                let size_of_extra = std::mem::size_of_val(&self.extra);
+                if size_of_extra > 20 {
+                    error!(
+                        "Username registration type transaction {}'s extra ({}) too large, {} > 20",
+                        self.hash, self.extra, size_of_extra
+                    );
+                    return Err(TransactionValidationErrors::ExtraTooLarge);
+                }
+                if self.gas
+                    < (TX_GAS as u64
+                        + ((GAS_PER_EXTRA_BYTE_NORMAL / 2) as u64 * self.extra.len() as u64))
+                {
+                    error!("Transaction {} gas low", self.hash);
+                    return Err(TransactionValidationErrors::LowGas);
+                }
+                if self.max_gas < self.gas {
+                    error!(
+                        "Transaction {} max_gas expended, max_gas={}, used_gas={}",
+                        self.hash, self.max_gas, self.gas
+                    );
+                    return Err(TransactionValidationErrors::MaxGasExpended);
+                }
+                if self.unlock_time != 0 {
+                    return Err(TransactionValidationErrors::UnsupportedType); // TODO: Implement unlock time
+                }
+            }
+            'b' => {
+                let size_of_extra = std::mem::size_of_val(&self.extra);
+                if size_of_extra > 100 {
+                    error!(
+                        "Burn type transaction {}'s extra ({}) too large, {} > 100",
+                        self.hash, self.extra, size_of_extra
+                    );
+                    return Err(TransactionValidationErrors::ExtraTooLarge);
+                }
+                if self.amount < 1 {
+                    error!("Burn transaction {} amount too small", self.hash);
+                    return Err(TransactionValidationErrors::InsufficentAmount);
+                }
+                if sender_account.balance < (self.amount + (self.gas * self.gas_price)) {
+                    error!("Sender {} of transaction {}'s balance too low, amount {} fee={} ({} * {}), required delta={}", sender_account.public_key, self.hash, self.amount, self.gas * self.gas_price, self.gas , self.gas_price, (self.amount + (self.gas * self.gas_price)) - sender_account.balance);
+                    return Err(TransactionValidationErrors::InsufficentBalance);
+                }
+                if self.gas
+                    < (TX_GAS as u64 + (GAS_PER_EXTRA_BYTE_NORMAL as u64 * self.extra.len() as u64))
+                {
+                    error!("Transaction {} gas low", self.hash);
+                    return Err(TransactionValidationErrors::LowGas);
+                }
+                if self.max_gas < self.gas {
+                    error!(
+                        "Transaction {} max_gas expended, max_gas={}, used_gas={}",
+                        self.hash, self.max_gas, self.gas
+                    );
+                    return Err(TransactionValidationErrors::MaxGasExpended);
+                }
+                if self.unlock_time != 0 {
+                    return Err(TransactionValidationErrors::UnsupportedType); // TODO: Implement unlock time
+                }
+            }
+            _ => {
+                error!("Transaction {} has unhandled type {}", self.hash, self.flag);
+                return Err(TransactionValidationErrors::UnsupportedType);
+            }
+        }
+        if self.timestamp - (config().transaction_timestamp_max_offset as u64)
+            > SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_millis() as u64
+        {
+            return Err(TransactionValidationErrors::BadTimestamp);
+        }
+        if self.access_key.is_empty() {
+            // eg if it was sent using the 'main' wallet
+            match bs58::decode(&sender_account.public_key.to_owned()).into_vec() {
+                // decode the base58 encoded sender public key into a vec
+                Ok(peer_public_key_bytes) => {
+                    let peer_public_key = signature::UnparsedPublicKey::new(
+                        &signature::ED25519,
+                        peer_public_key_bytes,
+                    );
+                    match peer_public_key.verify(
+                        // validate the signature
+                        self.hash.as_bytes(),
+                        &bs58::decode(&(self.signature).to_owned())
+                            .into_vec()
+                            .unwrap(),
+                    ) {
+                        Ok(()) => {}
+                        _ => {
+                            error!(
+                                "Transaction {} has bad signature {} (for public_key {})",
+                                self.hash, self.signature, sender_account.public_key
+                            );
+                            return Err(TransactionValidationErrors::BadSignature);
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!(
+                        "Transaction {} has bad publickey {}, decoding gave error={}",
+                        self.hash, sender_account.public_key, e
+                    );
+                    return Err(TransactionValidationErrors::BadPublicKey);
+                }
+            }
+        } else {
+            // this txn was sent using an access key
+            let mut key_to_use: Accesskey = Accesskey::default();
+            for key in sender_account.access_keys {
+                // try and find the access ley
+                if self.access_key == key.key {
+                    // TODO: Change Account.access_keys to a hashmap to allow faster key lookup
+                    key_to_use = key;
+                }
+            }
+            if key_to_use == Accesskey::default() {
+                // if we did not find it, return an error
+                return Err(TransactionValidationErrors::AccesskeyMissing);
+            } else if key_to_use.allowance < self.amount + (self.gas * self.gas_price)
+                && self.flag != 'c'
+            {
+                // check if this access keys 'allowance' is sufficent to cover this transaction (we have already checked the parent accounts balance)
+                error!("Access key {} has insufficent balance to cover txn {}, allowance {}, required {}", self.access_key, self.hash, key_to_use.allowance,  self.amount + (self.gas * self.gas_price));
+                return Err(TransactionValidationErrors::InsufficentBalance);
+            } else {
+                match bs58::decode(&self.access_key.to_owned()).into_vec() {
+                    Ok(peer_public_key_bytes) => {
+                        let peer_public_key = signature::UnparsedPublicKey::new(
+                            &signature::ED25519,
+                            peer_public_key_bytes,
+                        );
+                        match peer_public_key.verify(
+                            self.hash.as_bytes(),
+                            &bs58::decode(&(self.signature).to_owned())
+                                .into_vec()
+                                .unwrap(),
+                        ) {
+                            Ok(()) => {}
+                            _ => {
+                                error!(
+                                    "Transaction {} has bad signature {} (for access_key = {})",
+                                    self.hash, self.signature, self.access_key
+                                );
+                                return Err(TransactionValidationErrors::BadSignature);
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        error!(
+                            "Transaction {} has bad publickey {}, deocding gave error={}",
+                            self.hash, self.access_key, e
+                        );
+                        return Err(TransactionValidationErrors::BadPublicKey);
+                    }
+                }
+            }
+        }
+        trace!(
+            "Finished validating txn, took={} ms",
+            SystemTime::now()
+                .duration_since(start)
+                .expect("time went backwars ono")
+                .as_millis()
+        );
+        Ok(())
+    }
+
+    pub fn valid_old(&self, recieve: bool) -> Result<(), TransactionValidationErrors> {
         trace!("Validating txn with hash: {}", self.hash);
         let acc: Account = open_or_create(&self.sender_key);
-        let txn_count = avrio_database::get_data(
+        let txn_count = get_data(
             config().db_path
                 + &"/chains/".to_owned()
                 + &self.sender_key
@@ -389,7 +723,7 @@ impl Transaction {
     }
 
     pub fn validate_transaction(&self) -> bool {
-        self.valid(false).is_ok()
+        self.valid().is_ok()
     }
 
     pub fn hash(&mut self) {
