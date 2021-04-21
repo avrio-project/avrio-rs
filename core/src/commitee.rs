a6776befb9456248b6baee6ff814db97e25626ae
LeoCornelius <leocornelius019@gmail.com>
2021-04-21 19:16:04 +0100

    add calculate address range code

--- core/src/commitee.rs
+++ core/src/commitee.rs
@@ -1,7 +1,9 @@
 //use avrio_config::config;
 use avrio_crypto::{generate_keypair, raw_hash, Hashable};
+use bigdecimal::BigDecimal;
 use log::*;
 use serde::{Deserialize, Serialize};
+use std::ops::RangeInclusive;
 #[derive(Default, Debug, Serialize, Deserialize)]
 pub struct Comitee {
     pub index: u64,
@@ -9,12 +11,38 @@ pub struct Comitee {
     pub hash: String,
 }
 pub fn sort_full_list(full_list: &mut Vec<String>, epoch_salt: u64) {
+    // TODO: Fisher-yates shuffle
     full_list.sort_by(|a, b| {
         raw_hash(&(a.clone() + &epoch_salt.to_string()))
             .cmp(&raw_hash(&(b.clone() + &epoch_salt.to_string())))
     }); // sort the inital vector alphabeticly but with the node's pubkey hashed with epoch salt to give some dermanistic randomness
 }
 impl Comitee {
+    pub fn calculate_address_range(&self, number_of_committees: u64) -> RangeInclusive<BigDecimal> {
+        // We know there are 58^44 (389790920000000000000000000000000000000000000000000000000000000000000000000000) diffrent posible publickeys (base58 with len of 44), so the number of addresses per shard = (58^44)/(k-1)
+        // k = total number (including 0 committee hence the - 1) of committees
+        // to figure out the address space for comitee n (n = 0 means commitee 1, n = 1 means commitee 2 etc as we ignore the consensus comitee) you just do:
+        // n*((58^44)/(k-1))..=*((58^44)/(k-1))-((k-n)*((58^44)/(k-1)))
+        // if you let j = (58^44)/(k-1) this can simply be written as:
+        //nj..=j-(j(k-(n+1)))
+        let k = BigDecimal::from(number_of_committees - 1);
+        let j = BigDecimal::from(58 ^ 44) / k.clone(); // not a decimal but bigdecimal works to store integers too
+        if self.index != 0 {
+            let n = BigDecimal::from(self.index - 1);
+            n * j.clone()..=j.clone() - (j * (k - BigDecimal::from(self.index)))
+        } else {
+            BigDecimal::from(0)..=BigDecimal::from(0)
+        }
+    }
+    pub fn manages_address(&self, address: &String) -> Result<bool, Box<dyn std::error::Error>> {
+        if self.index == 0 {
+            return Ok(false);
+        }
+        let address_hex = hex::encode(bs58::decode(address).into_vec()?);
+        let address_numerical = i64::from_str_radix(&address_hex, 16)?.abs();
+        let address_bigdec = BigDecimal::from(address_numerical);
+        Ok(self.calculate_address_range(2).contains(&address_bigdec))
+    }
     pub fn form_comitees(
         sorted_list: &mut Vec<String>,
         excluded_nodes: &mut Vec<String>,
